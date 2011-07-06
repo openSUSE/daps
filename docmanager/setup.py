a735d7f0d222c4cb8195f2031e4c59302fa45bfc
Thomas Schraitle <tom_schr@web.de>
2011-07-06 13:45:52 +0000

    - Used setuptools instead of distutils; - Added more categories
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@16 2c1a2506-8097-4b87-b405-3e90b480097b

--- docmanager/setup.py
+++ docmanager/setup.py
@@ -1,30 +1,51 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+import os
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
-   name="docmanager",
-   version="2.0",
-   description="Manages Doc Files through SVN Properties",
-   author="Thomas Schraitle",
-   author_email="toms@opensuse.org",
-   url="https://svn.berlios.de/svnroot/repos/opensuse-doc/trunk/tools/daps/docmanager",
-   license="LGPL",
-   # packages=[],
-   # long_description=read("README"),
+  name="docmanager",
+  version="2.0",
+  author="Thomas Schraitle",
+  author_email="toms@opensuse.org",
+  url="https://svn.berlios.de/svnroot/repos/opensuse-doc/trunk/tools/daps/docmanager",
+  license="LGPL",
+  keywords="SVN properties status",
+   # packages=['dm'],
+  packages=find_packages(),
+  description="Manages Doc Files through SVN Properties",
+  long_description=read("README"),
    
-   py_modules=["dm"],  
-   script_name='docmanager2.py',    #[, "dm.py"]
-   classifiers=[
+   # py_modules=["dm"],  
+  script_name='docmanager2.py',    #[, "dm.py"]
+  classifiers=[
     "Intended Audience :: Developers",
     "Intended Audience :: Other Audience",
     "Development Status :: 4 - Beta",
     "Topic :: Utilities",
     "Programming Language :: Python",
     "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
-    # "Topic :: Text Processing",    
     "Topic :: Software Development :: Libraries :: Python Modules",
     "Topic :: Software Development :: Documentation",
+    "Topic :: Software Development :: Quality Assurance",
+    "Topic :: Software Development :: Version Control",
+    # "Topic :: Text Processing",    
+    "Topic :: Text Processing :: Markup :: XML",
+  ],
+   install_requires=[
+    'setuptools',
+    'lxml',
+    'optparse',
   ],
 )
