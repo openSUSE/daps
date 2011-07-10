8dc3161263d492acce6bd9d2a4a32f7aa46eb3f3
Thomas Schraitle <tom_schr@web.de>
2011-07-10 17:03:57 +0000

    - Class CleanCommand: Corrected indentation - Class TestCommand: Used existing user_options from super class;   disabled run()
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@62 2c1a2506-8097-4b87-b405-3e90b480097b

--- docmanager/setuptools_dmutils/__init__.py
+++ docmanager/setuptools_dmutils/__init__.py
@@ -11,38 +11,38 @@ __all__=["CleanCommand", "TestCommand"]
 
 
 class CleanCommand(Command):
-    description = "custom clean command that forcefully removes dist/build directories"
-    user_options = []
-    def initialize_options(self):
-        self.cwd = None
-    def finalize_options(self):
-        self.cwd = os.getcwd()
-    def run(self):
-        assert os.getcwd() == self.cwd, 'Must be in package root: %s' % self.cwd
-        os.system('rm -rf ./build ./dist')
+   """Command to clean the directory"""
+   description = "custom clean command that forcefully removes dist/build directories"
+   user_options = []
+   def initialize_options(self):
+      self.cwd = None
+   def finalize_options(self):
+      self.cwd = os.getcwd()
+   def run(self):
+      assert os.getcwd() == self.cwd, 'Must be in package root: %s' % self.cwd
+      os.system('rm -rf ./build ./dist')
 
 class TestCommand(_test):
    """Command to run unit tests after in-place build"""
    description = "Run unittests"
 
-   user_options = [
-      ('test-module=','m', "Run 'test_suite' in specified module"),
-      ('test-suite=','s',  "Test suite to run (e.g. 'some_module.test_suite')"),
+   user_options = _test.user_options + [
+      # Merge with our own:
       ('svnrepo=',     None, "SVN root repository"),
       ('workingrepo=', None, "Working directory"),
    ]
-   
+      
    def initialize_options(self):
-      _test.initialize_options(self)
+      _test.initialize_options(self)      
       self.svnrepo = None
       self.workingrepo = None
       
    def finalize_options(self):
       _test.finalize_options(self)
       self.test_args.extend(["--svnrepo", self.svnrepo])
-      print "finalize_options", self, self.__dict__
+      print "finalize_options", self, self.__dict__, sys.path
       
-   def run(self):
-      _test.run(self)
-      print "Running mytest..."
+   #def run(self):
+   #   _test.run(self)
+   #   print "Running mytest..."
 
