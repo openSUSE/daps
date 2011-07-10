519c34913afe41a40078551d82c7d39a815583fb
Thomas Schraitle <tom_schr@web.de>
2011-07-10 19:40:46 +0000

    Implemented ManpageCommand
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@64 2c1a2506-8097-4b87-b405-3e90b480097b

--- docmanager/setuptools_dmutils/__init__.py
+++ docmanager/setuptools_dmutils/__init__.py
@@ -2,16 +2,19 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import os
+import os, os.path
 
 from setuptools import Command
 from setuptools.command.test import test  as _test
+from setuptools.command.build_py import build_py
 
-__all__=["CleanCommand", "TestCommand"]
+__all__=["CleanCommand", "TestCommand", "ManpageCommand"]
+
+
 
 
 class CleanCommand(Command):
-   """Command to clean the directory"""
+   """clean build and dist directory"""
    description = "custom clean command that forcefully removes dist/build directories"
    user_options = []
    def initialize_options(self):
@@ -23,8 +26,8 @@ class CleanCommand(Command):
       os.system('rm -rf ./build ./dist')
 
 class TestCommand(_test):
-   """Command to run unit tests after in-place build"""
-   description = "Run unittests"
+   """run unit tests after in-place build"""
+   description = __doc__
 
    user_options = _test.user_options + [
       # Merge with our own:
@@ -46,3 +49,38 @@ class TestCommand(_test):
    #   _test.run(self)
    #   print "Running mytest..."
 
+class ManpageCommand(Command):
+   """creates manpage from DocBook source"""
+   description = __doc__
+
+   user_options = [
+      ('xml=',  None, "XML file in DocBook format"),
+      ('xslt=', None, "XSLT stylesheet"),
+      #('workingrepo=', None, "Working directory"),
+   ]
+   
+   def initialize_options(self):
+      self.xml=None
+      self.xslt=None
+      
+   def finalize_options(self):
+      if not self.xml:
+         self.xml = "doc/docmanager.xml"
+      if not self.xslt:
+         self.xslt="/usr/share/xml/docbook/stylesheet/nwalsh/current/manpages/docbook.xsl"
+      
+      print "verbose:", self.verbose
+      if not self.verbose:
+         print dir(self)
+         self.dump_options()
+      self.ensure_filename('xml')
+   
+   def run(self):
+      cmd="xsltproc --stringparam man.output.base.dir '%s/' " \
+          "--stringparam man.output.subdirs.enabled 0 " \
+          "--stringparam man.output.in.separate.dir 1 " \
+          "%s %s" % (os.path.dirname(self.xml), self.xslt, self.xml)
+      res=os.system(cmd)
+      if not self.verbose:
+         print "  %s -> %s" % (cmd, res)
+   
