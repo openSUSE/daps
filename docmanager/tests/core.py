0fba13c30ef85e5241eef782d85923483866ed68
Thomas Schraitle <tom_schr@web.de>
2011-07-11 14:51:22 +0000

    Added a core module which is does not have any testcases, but used as a helper
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@76 2c1a2506-8097-4b87-b405-3e90b480097b

---
+++ docmanager/tests/core.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import os, os.path
+
+try:
+  import unittest2 as unittest
+except ImportError:
+  import unittest
+
+
+def discover(globaldict, prefix="Test_", module=__file__):
+  """Discovers all classes in a module, which name starts with 'prefix' and are
+     derived from unittest.TestCase
+  """
+  suite = unittest.TestSuite()
+  
+  testcls = [ k for i, k in globaldict.items() \
+              if i.startswith(prefix) and issubclass(k, unittest.TestCase) ]
+  print(">>>  %s: Found %s testcases" % ( os.path.basename(module), len(testcls) ))
+  
+  for u in testcls:
+    case = unittest.TestLoader().loadTestsFromTestCase(u)
+    suite.addTests(case)
+  
+  return suite
