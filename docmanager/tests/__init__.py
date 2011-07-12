093fee72ff2f87de24cb2700d91786169a8049f5
Thomas Schraitle <tom_schr@web.de>
2011-07-12 09:47:32 +0000

    Reduced file as most of it is unnecessary due to nosetest
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@85 2c1a2506-8097-4b87-b405-3e90b480097b

--- docmanager/tests/__init__.py
+++ docmanager/tests/__init__.py
@@ -1,49 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 import sys
-try:
-  import unittest2 as unittest
-except ImportError:
-  import unittest
+import os, os.path
 
-from core import log
+from core import *
 
-print("----- Test Suite for DocManager -----")
+# SVNREPO="/var/tmp/docmanagersvn"
+# WORKINGREPO="/var/tmp/docmanager"
 
 
-def get_name_from_path(path):
-  # Taken from unittest2.loader
-  path = os.path.splitext(os.path.normpath(path))[0]
-  _relpath = os.path.relpath(path, os.getcwd())
-  assert not os.path.isabs(_relpath), "Path must be within the project"
-  assert not _relpath.startswith('..'), "Path must be within the project"
-  name = _relpath.replace(os.path.sep, '.')
-  return name
+def setUpPackage():
+  #FIXME: Create SVN repository
+  pass
 
-
-def suite( pattern="tests/test_*.py"):
-  """Returns a test suite for DocManager"""
-  # from tests import test_dm
-  import glob
-  suite = unittest.TestSuite()  
-  # Our defaults:
-  args={'svnrepo':    "/var/tmp/docmanagersvn",
-        'workingdir': "/var/tmp/docmanager",
-       }
-  
-  # Use pattern:
-  testpy=glob.glob(pattern)
-
-  # Iterate through all our found test cases and load them into our suite:
-  for path in testpy:
-    name = get_name_from_path(path)
-    print "> Loading %s" % name
-    case = unittest.TestLoader().loadTestsFromName(name)
-    suite.addTests(case)
-  
-  return suite
-
-#if __name__=="__main__":
-  #unittest.main()
