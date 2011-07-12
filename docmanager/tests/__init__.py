3a25c35a6b016cb07a28b81e5cb3952eb0d73c0e
Thomas Schraitle <tom_schr@web.de>
2011-07-12 12:36:13 +0000

    Removed obsolete comments
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@98 2c1a2506-8097-4b87-b405-3e90b480097b

--- docmanager/tests/__init__.py
+++ docmanager/tests/__init__.py
@@ -14,11 +14,6 @@ def setUpPackage():
   if not path.exists(SVNREPO) or not path.exists(WORKINGREPO):
     log.error("SVN repository '%s' does not exists!" % SVNREPO)
     d=path.relpath(path.abspath(path.dirname(__file__)))
-    # "svnadmin create %s" % SVNREPO
-    # "svn export %s %s.tmp" % (TESTROOT, WORKINGREPO)
-    # "svn import %s.tmp file://%s -m'Initial import'" % (WORKINGREPO, SVNREPO)
-    # shutil.rmtree("%s.tmp" %  WORKINGREPO)
-    # "svn checkout file://%s %s" % (SVNREPO, WORKINGREPO)
     
     res = os.system(path.join(d, "init-svn.sh --svnrepo %s " \
       "--workingrepo %s "\
