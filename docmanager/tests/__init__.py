0b5bb10a2c78af24042d4f1d093ce1e5e25ad2de
Thomas Schraitle <tom_schr@web.de>
2011-07-12 12:35:25 +0000

    Cleaned up, calling init-svn.sh Shell script
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@96 2c1a2506-8097-4b87-b405-3e90b480097b

--- docmanager/tests/__init__.py
+++ docmanager/tests/__init__.py
@@ -2,15 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import os, os.path
+import os
+from os import path
 
 from core import *
-
-# SVNREPO="/var/tmp/docmanagersvn"
-# WORKINGREPO="/var/tmp/docmanager"
+import subprocess
 
 
 def setUpPackage():
-  #FIXME: Create SVN repository
-  pass
-
+  # Create SVN repository
+  if not path.exists(SVNREPO) or not path.exists(WORKINGREPO):
+    log.error("SVN repository '%s' does not exists!" % SVNREPO)
+    d=path.relpath(path.abspath(path.dirname(__file__)))
+    # "svnadmin create %s" % SVNREPO
+    # "svn export %s %s.tmp" % (TESTROOT, WORKINGREPO)
+    # "svn import %s.tmp file://%s -m'Initial import'" % (WORKINGREPO, SVNREPO)
+    # shutil.rmtree("%s.tmp" %  WORKINGREPO)
+    # "svn checkout file://%s %s" % (SVNREPO, WORKINGREPO)
+    
+    res = os.system(path.join(d, "init-svn.sh --svnrepo %s " \
+      "--workingrepo %s "\
+      "--testroot %s" % (SVNREPO, WORKINGREPO, TESTROOT)))
+    log.error("Result of system: %s" % res)
