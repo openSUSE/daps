d691fd9a95e34031f741d911c63d3d67e48a5250
Thomas Schraitle <tom_schr@web.de>
2011-07-13 06:39:31 +0000

    Enhanced testcase with os.path.exists and os.path.isdir checks
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@120 2c1a2506-8097-4b87-b405-3e90b480097b

--- docmanager/tests/test_00svn.py
+++ docmanager/tests/test_00svn.py
@@ -16,17 +16,37 @@ except ImportError:
 from core import *
 
 class TestSVNRepository(unittest.TestCase):
+  """Checks structure of SVN repository"""
   
   def test_00svnrepo_exists(self):
     """Checks, if SVN repository exists"""
     self.assertTrue(path.exists(SVNREPO))
+  
+  def test_00svnrepo_isdir(self):
+    """Checks, if SVN repository is a directory"""
+    self.assertTrue(path.isdir(SVNREPO))
     
   def test_00svnworkdir_exists(self):
-    """Checks, if SVN directory exists"""
+    """Checks, if SVN working directory exists"""
     self.assertTrue(path.exists(WORKINGREPO))
+    
+  def test_00svnworkdir_isdir(self):
+    """Checks, if SVN working directory is a directory"""
+    self.assertTrue(path.isdir(WORKINGREPO))
+    
+  def test_00svnrepo_containsxmldir(self):
+    """Checks, if SVN working directory contains a subdirectory 'xml'"""
+    self.assertTrue(path.exists(path.join(WORKINGREPO, 'xml')))
+    
+  def test_00svnrepo_xmlisdir(self):
+    """Checks, if SVN working directory contains a subdirectory 'xml' and is a directory"""
+    self.assertTrue(path.isdir(path.join(WORKINGREPO, 'xml')))
   
   def test_dotsvn(self):
     """Checks, if the .svn directory is available"""
     self.assertTrue(path.exists(path.join(WORKINGREPO, '.svn')))
-    
+
+if __name__ == "__main__":
+  unittest.main()
+
 # EOF
