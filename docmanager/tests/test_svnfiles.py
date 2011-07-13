e88f11ce56f6183164b33e1abbdbf8eeaf30546d
Thomas Schraitle <tom_schr@web.de>
2011-07-13 06:44:37 +0000

    Included test_SVNFile_getinfo
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@122 2c1a2506-8097-4b87-b405-3e90b480097b

--- docmanager/tests/test_svnfiles.py
+++ docmanager/tests/test_svnfiles.py
@@ -10,9 +10,22 @@ from os import path
 
 from core import *
 
+from dm.base   import SVNFile
 
 class SVNFiles(unittest.TestCase):
   """ Testcase for SVNFile class """
+  @classmethod
+  def setUpClass(cls):
+    """Setups the class, used for all testcases"""
+    cls.filename = "test_01.xml"
+    cls.fullfilename = path.join(WORKINGREPO, "xml", cls.filename)
+    cls.svn = SVNFile(cls.fullfilename)
+    
+  def setUp(self):
+    self.c = self.__class__
+    self.svn = self.c.svn
+    self.filename = self.c.filename
+  
   def test_compareRootAndWorkingXMLFiles(self):
     """Create two lists and compare them: original from ROOT dir and from SVN working dir"""
     xmldir = path.join(WORKINGREPO, 'xml')
@@ -22,7 +35,14 @@ class SVNFiles(unittest.TestCase):
     roottestxml = [ f for f in os.listdir(roottestdir) if path.isfile(path.join(roottestdir,f)) and f.endswith(".xml")]
     
     self.assertListEqual(roottestxml, xml)
-    
+  
+  def test_SVNFile_getinfo(self):
+    """Checks if SVNFile.getinfo() return != '' """
+    info = self.svn.getinfo()
+    self.failUnless( info != '', "Could not get svn info for '%s'" % self.filename)
+    return True
+   
+  
   def foo(self):
     for x in xml:
       output=subprocess.check_output("svn pl -v --xml %s" % x, shell=True)
