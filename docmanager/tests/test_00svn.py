0c1c2bb9c105a0ffdfabb33683c8f7d1a983889e
Thomas Schraitle <tom_schr@web.de>
2011-07-13 06:40:32 +0000

    Removed obsolete import of unittest as it is imported from core now
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@121 2c1a2506-8097-4b87-b405-3e90b480097b

--- docmanager/tests/test_00svn.py
+++ docmanager/tests/test_00svn.py
@@ -7,11 +7,6 @@ __author__="Thomas Schraitle <toms AT opensuse DOT org>"
 import sys
 import os
 from os import path
-
-try:
-  import unittest2 as unittest
-except ImportError:
-  import unittest
   
 from core import *
 
