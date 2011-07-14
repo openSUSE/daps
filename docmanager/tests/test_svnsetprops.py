59a19eb0680f0f28868977c26c8094079c7f11ce
Thomas Schraitle <tom_schr@web.de>
2011-07-14 15:40:57 +0000

    First test of setting properties
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@165 2c1a2506-8097-4b87-b405-3e90b480097b

--- docmanager/tests/test_svnsetprops.py
+++ docmanager/tests/test_svnsetprops.py
@@ -9,7 +9,7 @@ import os
 from os import path
 
 from core import *
-
+from dm.commandline import main, climain
 from dm.base import SVNFile
 
 
@@ -27,7 +27,27 @@ class SVNSetProperties(unittest.TestCase):
   def setUp(self):
     self.c = self.__class__
     self.svn = self.c.svn
+    self.filename = self.c.filename
 
+  def test_setMaintainer(self):
+    """Change maintainer to 'tux'"""
+    user = "tux"
+    
+    pwd = os.getcwd()
+    os.chdir( WORKINGREPO )# 
+    sys.argv=["testy.py", "--force",
+                "ds", "-o", "/tmp/.dm-test.log",
+                "--set-maintainer", user,
+                "xml/%s" % self.filename ]
+    res = main()
+    cmd="svn pg doc:maintainer xml/%s" % self.filename
+    proc=subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    out, err = proc.communicate()
+    
+    self.assertEqual( out.strip(), user, "Unexpected maintainer")
+    os.chdir(pwd)
+    
+    
 
 if __name__ == "__main__":
   unittest.main()
