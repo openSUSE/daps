3ee1fbb76c77387a426b4fc2b1dd82d280234c6b
Thomas Schraitle <tom_schr@web.de>
2011-07-14 14:38:58 +0000

    Added GPL license header
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@161 2c1a2506-8097-4b87-b405-3e90b480097b

--- docmanager/dm/base.py
+++ docmanager/dm/base.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
+#
+# This file is part of the docmanager distribution.
+#
+# docmanager is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 2 of the License, or (at your option) any
+# later version.
+#
+# docmanager is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __version__="$Id: base.py 45614 2009-11-17 20:15:17Z jw $"
 __author__="Thomas Schraitle <thomas.schraitle@suse.de>"
