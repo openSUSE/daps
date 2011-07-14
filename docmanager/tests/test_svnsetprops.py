a08edfc794ebbf4b9131639b28436b6bf0f26303
Thomas Schraitle <tom_schr@web.de>
2011-07-14 15:54:31 +0000

    Added new testcases test_setProofing, test_setProofed, test_setLocdrop, and test_setComments
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@167 2c1a2506-8097-4b87-b405-3e90b480097b

--- docmanager/tests/test_svnsetprops.py
+++ docmanager/tests/test_svnsetprops.py
@@ -76,6 +76,76 @@ class SVNSetProperties(unittest.TestCase):
     
     self.assertEqual( out.strip(), "edited", "Unexpected doc:status")
     os.chdir(pwd)
+    
+  def test_setProofing(self):
+    """Change doc:status to 'proofing'"""
+    
+    pwd = os.getcwd()
+    os.chdir( WORKINGREPO )# 
+    sys.argv=["testy.py", "--force",
+                "ds", "-o", "/tmp/.dm-test.log",
+                "--set-proofing",
+                "xml/%s" % self.filename ]
+    res = main()
+    cmd="svn pg doc:status xml/%s" % self.filename
+    proc=subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    out, err = proc.communicate()
+    
+    self.assertEqual( out.strip(), "proofing", "Unexpected doc:status")
+    os.chdir(pwd)
+    
+  def test_setProofed(self):
+    """Change doc:status to 'proofed'"""
+    
+    pwd = os.getcwd()
+    os.chdir( WORKINGREPO )# 
+    sys.argv=["testy.py", "--force",
+                "ds", "-o", "/tmp/.dm-test.log",
+                "--set-proofed",
+                "xml/%s" % self.filename ]
+    res = main()
+    cmd="svn pg doc:status xml/%s" % self.filename
+    proc=subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    out, err = proc.communicate()
+    
+    self.assertEqual( out.strip(), "proofed", "Unexpected doc:status")
+    os.chdir(pwd)
+    
+  def test_setLocdrop(self):
+    """Change doc:status to 'locdrop'"""
+    
+    pwd = os.getcwd()
+    os.chdir( WORKINGREPO )# 
+    sys.argv=["testy.py", "--force",
+                "ds", "-o", "/tmp/.dm-test.log",
+                "--set-locdrop",
+                "xml/%s" % self.filename ]
+    res = main()
+    cmd="svn pg doc:status xml/%s" % self.filename
+    proc=subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    out, err = proc.communicate()
+    
+    self.assertEqual( out.strip(), "locdrop", "Unexpected doc:status")
+    os.chdir(pwd)
+
+  def test_setComments(self):
+    """Change doc:status to 'comments'"""
+    
+    pwd = os.getcwd()
+    os.chdir( WORKINGREPO )# 
+    sys.argv=["testy.py", "--force",
+                "ds", "-o", "/tmp/.dm-test.log",
+                "--set-comments",
+                "xml/%s" % self.filename ]
+    res = main()
+    cmd="svn pg doc:status xml/%s" % self.filename
+    proc=subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    out, err = proc.communicate()
+    
+    self.assertEqual( out.strip(), "comments", "Unexpected doc:status")
+    os.chdir(pwd)
+
+  
 
 if __name__ == "__main__":
   unittest.main()
