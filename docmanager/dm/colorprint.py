f5db299f7ea1cac5a55be32ddba67c0bd3fac78b
Thomas Schraitle <tom_schr@web.de>
2011-07-18 10:08:01 +0000

    Added license header
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@177 2c1a2506-8097-4b87-b405-3e90b480097b

--- docmanager/dm/colorprint.py
+++ docmanager/dm/colorprint.py
@@ -1,4 +1,19 @@
 # -*- coding: UTF-8 -*-
+#
+# This file is part of the docmanager distribution.
+#
+# docmanager is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 2 of the License, or (at your option) any
+# later version.
+#
+# docmanager is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
 
 __version__="$Revision: 99 $"
 __author__="Thomas Schraitle <thomas DOT schraitle AT suse DOT de>"
