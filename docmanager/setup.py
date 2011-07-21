4eaf60415db3f46eef96a34e4d25a439d18ee188
Thomas Schraitle <tom_schr@web.de>
2011-07-21 12:45:01 +0000

    0.9beta4a
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@238 2c1a2506-8097-4b87-b405-3e90b480097b

--- docmanager/setup.py
+++ docmanager/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 from setuptools_dmutils import CleanCommand, ManpageCommand
 
-version="0.9beta4"
+version="0.9beta4a"
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 
