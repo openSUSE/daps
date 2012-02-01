198f515321f5e3cbc27ef6b3c6f5798e4bfbcad5
Thomas Schraitle <tom_schr@web.de>
2012-02-01 07:45:11 +0000

    Removed obsolete lines of libtoolize (we are noarch)
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@615 2c1a2506-8097-4b87-b405-3e90b480097b

--- autogen.sh
+++ autogen.sh
@@ -6,7 +6,6 @@ echo -ne "\n*** Generating configure and Makefiles... *** \n\n"
 
 # Creating files which is expected by GNU projects:
 touch NEWS README AUTHORS ChangeLog
-# libtoolize --force && \
 aclocal && autoconf && automake --force-missing --add-missing
 
 echo -ne "\n\nFinished. Now type ./configure && make\n\n"
