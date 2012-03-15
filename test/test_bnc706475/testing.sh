5d915321016d17cda4a3480e4313c0344c75965b
Thomas Schraitle <tom_schr@web.de>
2012-03-15 15:19:51 +0000

    Make sourcing common.sh more robust
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@990 2c1a2506-8097-4b87-b405-3e90b480097b

--- test/test_bnc706475/testing.sh
+++ test/test_bnc706475/testing.sh
@@ -4,7 +4,11 @@
 # (C) 2012 Thomas Schraitle <toms@opensuse.org>
 #
 
-[[ -f common.sh ]] && source common.sh
+if [[ -f common.sh ]]; then
+  source common.sh
+elif [[ -f ../common.sh ]]; then
+  source ../common.sh
+fi
 
 test_X() {
   assertTrue "Not equal" "[[ 1 -eq 1 ]]"
--- test/test_doc/testing.sh
+++ test/test_doc/testing.sh
@@ -5,8 +5,70 @@
 # (C) 2012 Thomas Schraitle <toms@opensuse.org>
 #
 
-[[ -f common.sh ]] && source common.sh 
+if [[ -f common.sh ]]; then
+  source common.sh
+elif [[ -f ../common.sh ]]; then
+  source ../common.sh
+fi
 
+
+usage() {
+cat << EOF
+${0##*/} [OPTIONS...]
+DAPS Testing Framework
+
+Available Options:
+ -h, --help
+     Shows this help
+ -i, --dapsinit DAPSINIT
+     Absolute filename, points to the daps-init program
+ -D, --daps DAPS
+     Absolute filename, points to the daps script
+ -l, --logfile LOGFILE
+     Save result in LOGFILE (default $LOGFILE)
+ -k, --keeptemp
+     Do not delete the TEMPDIR directory, default ${DELTEMP}
+EOF
+exit 0
+}
+
+# ---------
+# Parsing Command Line Options
+if ! options=$(getopt -o h,i:,l:,D:,k  -l help,dapsinit:,logfile:,daps:,keeptemp -- "$@"); then
+    exit 1
+fi
+
+eval set -- "$options"
+
+while [ $# -gt 0  ]; do
+  case "$1" in                                                    
+    -h|--help) usage;;
+    -i|--dapsinit)
+        D=$(readlink -f $2)
+        [[ -f $D ]] || exit_on_error "$D does not exist"
+        DAPS_INIT=$D
+        shift
+        ;;
+    -l|--logfile)
+       # Make an *absolute* filename to avoid problems when 
+       # switching to other directories
+       LOGFILE=$(readlink -f $2)
+       shift
+       ;;
+    -D|--daps)
+       [[ -f $2 ]] || exit_on_error "$2 does not exist"
+       DAPS=$(readlink -f $2)
+       shift
+       ;;
+    -k|--keeptemp)
+       DELTEMP=0
+       ;;
+    (--) shift; break;;                                           
+    (-*) exit_on_error "${0##*/}: error - unrecognized option $1" ;; 
+    (*)  break;;
+  esac
+ shift
+done
 
 
 test_XYZ() {
