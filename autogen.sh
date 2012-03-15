6a32c9e970a0a674899097c2e21dd8cdb726aa4b
Frank Sundermeyer <fs@suse.de>
2012-03-15 10:12:10 +0000

    Version 1.0beta2
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@978 2c1a2506-8097-4b87-b405-3e90b480097b

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,6 @@ echo -ne "\n*** Generating configure and Makefiles... *** \n\n"
 
 # Creating files which is expected by GNU projects:
 touch NEWS README AUTHORS ChangeLog
-aclocal -I m4 && autoconf && automake --force-missing --add-missing
+aclocal -I m4 && autoconf && automake --force-missing --add-missing --copy
 
 echo -ne "\n\nFinished. Now type ./configure && make\n\n"
