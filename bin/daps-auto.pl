4b345fd5ada98e0c97a4411f07823fce62d5b2b6
Frank Sundermeyer <fs@suse.de>
2012-06-21 20:53:26 +0000

    Command line parsing, config file parsing, and building books according to config file works -- rsync still missing
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@1623 2c1a2506-8097-4b87-b405-3e90b480097b

--- bin/daps-auto.pl
+++ bin/daps-auto.pl
@@ -13,8 +13,6 @@ use Getopt::Long;
 use Config::IniFiles;
 use File::Basename;
 use File::Spec::Functions;
-use Data::Dumper;
-
 
 
 my $me = basename($0);
@@ -45,7 +43,7 @@ usage() if $help;
 @sections = split(/,/,join(',',@sections));
 
 # Check config file
-die "\033[31mNo valid config file specified:\n\t $config\033[0m\n" if ! -s $config;
+die "\e[31mNo valid config file specified:\n\t $config\e[0m\n" if ! -s $config;
 
 #-------------------------------------------------------------------
 # Read config file
@@ -62,7 +60,7 @@ my @sets          = (); # the sections from the config file
 my @vformats      = qw(color-pdf epub html htmlsingle pdf txt); # valid formats
 
 tie %config, 'Config::IniFiles', (-file => $config ) or
-    die "\033[31mPlease check the config file syntax.\033[0m\n";
+    die "\e[31mPlease check the config file syntax.\e[0m\n";
 @sets=tied (%config)->Sections;
 
 #
@@ -71,7 +69,7 @@ tie %config, 'Config::IniFiles', (-file => $config ) or
 
 # set buildroot
 if ( ! -d $config{general}{builddir} ) {
-    die "\033[31mInvalid builddir in section [general].\033[0m\n"
+    die "\e[31mInvalid builddir in section [general].\e[0m\n"
 } else {
     $builddir = $config{general}{builddir};
 }
@@ -83,10 +81,10 @@ if ( $config{general}{dapsroot} ne "" ) {
             $dapsroot = "$config{general}{dapsroot}";
             $dapsbin  = "$config{general}{dapsroot}/bin/daps";
         } else {
-            die "\033[31mdapsroot from section [general] does not contain bin/daps.\033[0m\n"
+            die "\e[31mdapsroot from section [general] does not contain bin/daps.\e[0m\n"
         }
     } else {
-       die "\033[31mInvalid dapsroot in section [general].\033[0m\n" 
+       die "\e[31mInvalid dapsroot in section [general].\e[0m\n" 
     }
 }
 
@@ -95,12 +93,6 @@ if ( $config{general}{dapsroot} ne "" ) {
 # the books
 #
 
-my $dapscmd = "";
-
-
-$dapscmd = "$dapsbin --vv --builddir=\"$builddir\"";
-$dapscmd .= " --dapsroot=\"$dapsroot\"" if $dapsroot ne "";
-
 foreach my $set (@sets) {
     next if $set eq "general"; # skip general section
     print "---------- $set ----------\n";
@@ -108,44 +100,75 @@ foreach my $set (@sets) {
     @formats = split /, */, $config{$set}{formats};
     # Working Direcory
     if ( ! -d $config{$set}{workdir} ) {
-        warn "\033[31mInvalid working directory in config for section [$set].\n->Skipping complete set [$set].\033[0m\n";
+        warn "\e[31mInvalid working directory in config for section [$set].\n->Skipping complete set [$set].\e[0m\n";
         next;
     }
-    # DC-files
-    foreach ( @dcfiles ) {
-        print "---------- $_ ----------\n";
-        my $dc = catfile($config{$set}{workdir}, $_);
-        my $dc_dapscmd = "$dapscmd";
-        if ( ! -f $dc ) {
-            warn "\033[31mInvalid DC-file \"$_\" in config for section [$set].\n-> Skipping $_\033[0m.\n";
-            next
+    # formats
+    foreach my $format ( @formats ) {
+        if ( ! grep { $_ eq $format } @vformats ) {
+            warn "\e[31mInvalid format \"$format\" in config for section [$set].\n-> Skipping $format.\e[0m\n";
+            next;
         } else {
-            $dc_dapscmd .= " --docconfig=\"$dc\"";
-            # formats
-            foreach my $format ( @formats ) {
-                my $format_dapscmd = "$dc_dapscmd";
-                if ( ! grep { $_ eq $format } @vformats ) {
-                    warn "\033[31mInvalid format \"$format\" in config for section [$set].\n-> Skipping $format.\033[0m\n";
+            foreach my $dcfile ( @dcfiles ) {
+                print "---------- $dcfile: \U$format ----------\n";
+                my $dcpath = catfile($config{$set}{workdir}, $dcfile);
+                if ( ! -f $dcpath ) {
+                    warn "\e[31mInvalid DC-file \"$dcfile\" in config for section [$set].\n-> Skipping $dcfile\e[0m.\n";
                     next;
                 } else {
-                    $format_dapscmd .= " $format";
-                    if ($config{$set}{bookname} ne "") {
-                        $dapscmd .= " --name=\"$config{$set}{bookname}\"";
+                    my $dapscmd    = '';
+                    my $dapserror  = '';
+                    my $dapsresult = '';
+                    my ($dapscmd, $dapslog) = set_daps_cmd_and_log($set,$dcpath, $format);
+                    print "$dapscmd\n";
+                    print "$dapslog\n";
+                    $dapsresult = `$dapscmd` and $dapserror = 1;
+                    if ( $dapserror ) {
+                        warn "\e[31mFailed to execute\n$dapscmd\nSee $dapslog for details.\e[0m\n";
+                        next;
+                    } else {
+                        print "\e[32m$dapsresult\e[0m";
                     }
-                    if ( $format =~ /^html.*/ or $format =~ /.*pdf$/ ) {
-                        $format_dapscmd .= " --remarks" if  $config{$set}{remarks};
-                        $format_dapscmd .= " --draft" if  $config{$set}{draft};
-                        $format_dapscmd .= " --meta" if  $config{$set}{meta};
-                    }
-                    print "$format_dapscmd\n";
                 }
             }
         }
     }
 }
 
-
 #-------------------------------------------------------------------
+
+#-----
+# Create the daps command and the logfile location
+# according to the data from the logfile
+sub set_daps_cmd_and_log {
+    my ($set,$dcpath, $format) = @_;
+    my $bookname;
+    my $dapscmd;
+    my $dapslog;
+
+    #-------------------
+    # daps command
+    #
+    $dapscmd =  "$dapsbin --builddir=\"$builddir\" --docconfig=\"$dcpath\"";
+    $dapscmd .= " --dapsroot=\"$dapsroot\"" if $dapsroot ne "";
+    $dapscmd .= " $format";
+    if ( $format =~ /^html.*/ or $format =~ /.*pdf$/ ) {
+        $dapscmd .= " --remarks" if  $config{$set}{remarks};
+        $dapscmd .= " --draft" if  $config{$set}{draft};
+        $dapscmd .= " --meta" if  $config{$set}{meta};
+        if ( $format =~ /^html.*/ ) {
+            $dapscmd .= " --static";
+        }
+    }    
+    #-------------------
+    # logfile
+    #
+    $bookname = basename ($dcpath);
+    $bookname =~ s/^DC-//;
+    $dapslog = catfile($builddir, $bookname, "log", "make_$format.log");
+    return ($dapscmd, $dapslog);
+}
+
 
 #-----
 # Help text
@@ -155,7 +178,7 @@ Usage: $me --config|-c FILE [OPTIONS]
 
        --config|-c FILE     : Path to config (ini) file
        --help|-h            : This help text
-       --sections|s SECTIONS: Comma seperated list of sections from ini file
+       --sections|s SECTIONS: Comma separated list of sections from ini file
                               to use (skip all others). Cann also be specified
                               with multiple instances of --sections:
                               --sections SECT1,SECT2
