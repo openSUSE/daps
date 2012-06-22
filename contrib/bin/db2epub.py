51d3060c50560f09db5ded1fc12726a486f8469b
Thomas Schraitle <tom_schr@web.de>
2012-06-22 14:53:15 +0000

    Implemented Formatter class IndentedHelpFormatterWithNL and added description
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@1632 2c1a2506-8097-4b87-b405-3e90b480097b

--- contrib/bin/db2epub.py
+++ contrib/bin/db2epub.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 """
-This program converts DocBook documents into .epub files
+EPUB is defined by the IDPF at www.idpf.org and is made up of 3 standards:
+* Open Publication Structure (OPS)
+* Open Packaging Format (OPF) 
+* Open Container Format (OCF)
 """
 
 __version__="0.1"
@@ -11,6 +14,7 @@ import os
 import optparse
 
 import logging
+import textwrap
 
 #IMPORTDIR=os.path.dirname(__file__)
 #sys.path.insert(0, IMPORTDIR)
@@ -23,9 +27,66 @@ log.addHandler(fh)
 log.setLevel(logging.INFO)
 log.addHandler(logging.StreamHandler(sys.stdout))
 
+class IndentedHelpFormatterWithNL(optparse.IndentedHelpFormatter):
+  def format_description(self, description):
+    if not description: return ""
+    desc_width = self.width - self.current_indent
+    indent = " "*self.current_indent
+# the above is still the same
+    bits = description.split('\n')
+    formatted_bits = [
+      textwrap.fill(bit,
+        desc_width,
+        initial_indent=indent,
+        subsequent_indent=indent)
+      for bit in bits]
+    result = "\n".join(formatted_bits) + "\n"
+    return result
+
+  def format_option(self, option):
+    # The help for each option consists of two parts:
+    #   * the opt strings and metavars
+    #   eg. ("-x", or "-fFILENAME, --file=FILENAME")
+    #   * the user-supplied help string
+    #   eg. ("turn on expert mode", "read data from FILENAME")
+    #
+    # If possible, we write both of these on the same line:
+    #   -x    turn on expert mode
+    #
+    # But if the opt string list is too long, we put the help
+    # string on a second line, indented to the same column it would
+    # start in if it fit on the first line.
+    #   -fFILENAME, --file=FILENAME
+    #       read data from FILENAME
+    result = []
+    opts = self.option_strings[option]
+    opt_width = self.help_position - self.current_indent - 2
+    if len(opts) > opt_width:
+      opts = "%*s%s\n" % (self.current_indent, "", opts)
+      indent_first = self.help_position
+    else: # start help on same line as opts
+      opts = "%*s%-*s  " % (self.current_indent, "", opt_width, opts)
+      indent_first = 0
+    result.append(opts)
+    if option.help:
+      help_text = self.expand_default(option)
+# Everything is the same up through here
+      help_lines = []
+      for para in help_text.split("\n"):
+        help_lines.extend(textwrap.wrap(para, self.help_width))
+# Everything is the same after here
+      result.append("%*s%s\n" % (
+        indent_first, "", help_lines[0]))
+      result.extend(["%*s%s\n" % (self.help_position, "", line)
+        for line in help_lines[1:]])
+    elif opts[-1] != "\n":
+      result.append("\n")
+    return "".join(result) 
 
 def main():
-  parser = optparse.OptionParser(version="%prog "+__version__, ) # epilog="See also "
+  parser = optparse.OptionParser(version="%prog "+__version__, 
+               formatter=IndentedHelpFormatterWithNL(),
+               description=__doc__) # epilog="See also "
   parser.add_option("-c", "--css",
      dest="CSSFILE",
      help="Use CSSFILE for CSS on generated XHTML")
