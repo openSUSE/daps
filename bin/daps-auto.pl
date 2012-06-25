f19281f67edcdcf916a60a7ac4557198a2a13fc8
Frank Sundermeyer <fs@suse.de>
2012-06-25 12:12:00 +0000

    Final (hopefully ;-) ) version of the auto-daps script
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@1639 2c1a2506-8097-4b87-b405-3e90b480097b

--- bin/daps-auto.pl
+++ bin/daps-auto.pl
@@ -1,11 +1,12 @@
 #!/usr/bin/perl -w
 # daps-auto.pl ---
 # Create documentation output with DAPS and rsync it to given servers
-# Needs a config file in ini style, see FIXME/sample.ini
+# Needs a config file in ini style, see
+# /usr/share/daps/init_templates/daps-auto_sample.ini
 #
 # Author: Frank Sundermeyer <fsundermeyer@opensuse.org>
 # Created: 20 Jun 2012
-# Version: 0.01
+# Version: 1.0
 
 use warnings;
 use strict;
@@ -19,6 +20,10 @@ use File::Spec::Functions;
 
 
 my $me = basename($0);
+my $builddir      = "";
+my $dapsbin       = "/usr/bin/daps";
+my $dapsroot      = "";
+
 $ENV{SHELL}="/bin/bash";
 $ENV{PATH}= ".:/usr/bin:/bin";
 
@@ -28,7 +33,9 @@ $ENV{PATH}= ".:/usr/bin:/bin";
 
 my $clean      = '';
 my $config     = '';
+my $debug      = '';
 my $help       = '';
+my $nocolor    = '';
 my $norsync    = 0;
 my $nosvnup    = 0;
 my @sections   = ();
@@ -37,7 +44,9 @@ my $verbose    = '';
 GetOptions (
     'clean'        => \$clean,
     'config|c=s'   => \$config,
+    'debug'        => \$debug,
     'help|h'       => \$help,
+    'nocolor'      => \$nocolor,
     'norsync'      => \$norsync,
     'nosvnup'      => \$nosvnup,
     'sections|s=s' => \@sections,
@@ -61,44 +70,63 @@ die "\e[31mNo valid config file specified:\n\t $config\e[0m\n" if ! -s $config;
 my $cfg = Config::IniFiles->new( -file => "$config" ) or
     die "\e[31mPlease check the config file syntax.\e[0m\n";
 
-
-my $builddir      = "";
-my $dapsbin       = "/usr/bin/daps";
-my $dapsroot      = "";
 my @dcfiles       = (); # all DC files from one set
 my @formats       = (); # all formats from one set
 my @sets          = (); # the sections from the config file
 my @vformats      = qw(color-pdf epub html htmlsingle pdf txt); # valid formats
 
-@sets = $cfg->Sections;
 
 #
 # test values and set variables
 #
 
+my $bcol = '';
+my $ecol = '';
+if ( ! $nocolor ) {
+    $bcol = "\e[31m";
+    $ecol = "\e[0m";
+}
+
+@sets = $cfg->Sections;
+if ( @sections ) {
+    # sections were specified onm the command line
+    foreach my $sect ( @sections ) {
+        if ( ! grep { $_ eq $sect } @sets ) {
+           die "${bcol}Invalid section specified on the command line.${ecol}\n" 
+       }
+    }
+    @sets = @sections;
+}
+
 # set buildroot
-if ( ! -d $cfg->val('general', 'builddir') ) {
-    die "\e[31mInvalid builddir \"$cfg->val(general, builddir)\" in section [general].\e[0m\n"
-} else {
-    $builddir = $cfg->val('general', 'builddir');
+$builddir = $cfg->val('general', 'builddir');
+if ( ! -d $builddir ) {
+    die "${bcol}Invalid builddir \"$builddir\" in section [general].${ecol}\n"
 }
 
 # set dapsroot, dapsbin
 if ( $cfg->val('general', 'dapsroot') ne "" ) {
-    if ( -d $cfg->val('general', 'dapsroot') ) {
-        if ( -x "$cfg->val('general', 'dapsroot')/bin/daps" ) {
-            $dapsroot = "$cfg->val('general', 'dapsroot')";
-            $dapsbin  = "$cfg->val('general', 'dapsroot')/bin/daps";
+    $dapsroot = $cfg->val('general', 'dapsroot');
+    if ( -d $dapsroot ) {
+        if ( -x "$dapsroot/bin/daps" ) {
+            $dapsbin  = "$dapsroot/bin/daps";
         } else {
-            die "\e[31mdapsroot from section [general] does not contain bin/daps.\e[0m\n"
+            die "${bcol}dapsroot \"$dapsroot\" from section [general] does not contain bin/daps.${ecol}\n"
         }
     } else {
-       die "\e[31mInvalid dapsroot in section [general].\e[0m\n" 
+       die "${bcol}Invalid dapsroot \"$dapsroot\" in section [general].${ecol}\n" 
     }
 }
 
-# clean up
+# verbose settings
+my $devnull = "";
+$verbose = 1 if $debug;
 
+if ( ! $verbose ) {
+    $devnull = " >/dev/null 2>&1";
+}
+
+# clean up
 if ( $clean ) {
     rmtree("$builddir");
     mkdir("$builddir",0755);
@@ -111,57 +139,69 @@ if ( $clean ) {
 #
 
 foreach my $set (@sets) {
-    next if $set eq "general"; # skip general section
-    print "---------- $set ----------\n";
+    # daps does not like spaces
+    my $to_rsync = 0;
+    next if "$set" eq "general"; # skip general section
+    print "$set:\n";
+    if ( $set =~ /.*\s.*/ ) {
+        warn "${bcol}Set names must not include whitespace.\n${ecol}Skipping set [$set].\n";
+        next
+    }
     @dcfiles = split /, */, $cfg->val("$set", 'dcfiles');
     @formats = split /, */, $cfg->val("$set", 'formats');
     my $workdir = $cfg->val("$set", 'workdir');
     # Working Direcory
     if ( ! -d $workdir ) {
-        warn "\e[31mInvalid working directory \"$workdir\"in config for section [$set].\n->Skipping complete set [$set].\e[0m\n";
+        warn "${bcol}Invalid working directory \"$workdir\"in config for section [$set].\n->Skipping set [$set].${ecol}\n";
         next;
     }
     if ( not $nosvnup) {
-        system("/usr/bin/svn up $workdir >/dev/null 2>&1") == 0
-            or warn "\e[31mFailed to svn update $workdir.\e[0m\n";
+        print "  Doing an svn up on\n  $workdir\n  " if $verbose;
+        system("/usr/bin/svn up $workdir$devnull") == 0
+            or warn "${bcol}Failed to svn update $workdir.${ecol}\n";
     }
     # formats
     foreach my $format ( @formats ) {
         if ( ! grep { $_ eq $format } @vformats ) {
-            warn "\e[31mInvalid format \"$format\" in config for section [$set].\n-> Skipping $format.\e[0m\n";
+            warn "${bcol}Invalid format \"$format\" in config for section [$set].\n-> Skipping ${format}.${ecol}\n";
             next;
         } else {
             foreach my $dcfile ( @dcfiles ) {
-                print "---------- $dcfile: \U$format ----------\n";
+                print "  * $dcfile: \U$format...";
                 my $dcpath = catfile("$workdir", "$dcfile");
                 if ( ! -f $dcpath ) {
-                    warn "\e[31mInvalid DC-file \"$dcfile\" in config for section [$set].\n-> Skipping $dcfile\e[0m.\n";
+                    warn "${bcol}Invalid DC-file \"$dcfile\" in config for section [$set].\n-> Skipping $dcfile${ecol}.\n";
                     next;
                 } else {
                     my $dapserror  = '';
                     my $dapsresult = '';
-                    my ($dapscmd, $dapslog) = set_daps_cmd_and_log($set, $dcpath, $format);
-                    print "$dapscmd\n";
-                    print "$dapslog\n";
+                    my ($dapscmd, $dapslog) = set_daps_cmd_and_log("$set", "$dcpath", "$format");
+                    print "\n    Command: $dapscmd\n    Logfile: $dapslog\n" if $verbose;
                     $dapsresult = `$dapscmd`;
                     chomp($dapsresult);
                     $dapserror = $?; # error occurred if != 0
                     
                     if ( $dapserror) {
-                        warn "\e[31mFailed to execute\n$dapscmd\nSee $dapslog for details.\e[0m\n";
+                        warn "${bcol}Failed to execute\n$dapscmd\nSee $dapslog for details.${ecol}\n";
                         next;
                     } else {
-                        print "\e[32m$dapsresult\n\e[0m";
+                        $to_rsync = 1;
+                        if ( $verbose ) {
+                            print "    $dapsresult\n";
+                        } else {
+                            print "OK\n";
+                        }
                         #
                         # Move non-html format results into subdirectory $format
                         #
                         if ( $format !~ /^html.*/ ) {
                             my $resultdir  = dirname($dapsresult);
                             my $format_dir = catdir($resultdir, $format);
+                            print "    Moving $dapsresult to $format_dir\n" if $verbose;
                             if ( ! -d $format_dir ) {
-                                mkdir($format_dir,0755) or warn "\e[31mFailed to create directory $format_dir.\e[0m\n";
+                                mkdir($format_dir,0755) or warn "${bcol}Failed to create directory $format_dir.${ecol}\n";
                             }
-                            move($dapsresult, $format_dir) or warn "\e[31mFailed to move $dapsresult to $format_dir.\e[0m\n";
+                            move($dapsresult, $format_dir) or warn "${bcol}Failed to move $dapsresult to $format_dir.${ecol}\n";
                         }
                  
                     }
@@ -169,7 +209,7 @@ foreach my $set (@sets) {
             }
         }
     }
-    if ( not $norsync) {
+    if ( not $norsync and $to_rsync ) {
         my $rsync_target;
         my $rsync_flags;
         if ( $cfg->exists("$set", 'rsync_target') ) {
@@ -182,8 +222,9 @@ foreach my $set (@sets) {
         } else {
             $rsync_flags = $cfg->val('general', 'rsync_flags');
         }
-        rsync($rsync_target,$rsync_flags, $set);
+        rsync("$rsync_target", "$rsync_flags", "$set");
     }
+    print "\n";
 }
 
 #-------------------------------------------------------------------
@@ -195,14 +236,14 @@ foreach my $set (@sets) {
 # according to the data from the logfile
 #
 sub set_daps_cmd_and_log {
-    my ($set,$dcpath, $format) = @_;
+    my ($set, $dcpath, $format) = @_;
     my $bookname;
     my $dapscmd;
     my $dapslog;
-    my $set_builddir = catdir($builddir, $set);
+    my $set_builddir = catdir($builddir, "$set");
 
     if ( ! -d $set_builddir ) {
-        mkdir($set_builddir, 0755) or die "\e[31mFailed to create directory $set_builddir\e[0m";
+        mkdir($set_builddir, 0755) or die "${bcol}Failed to create directory $set_builddir${ecol}";
     }
 
     #-------------------
@@ -210,6 +251,7 @@ sub set_daps_cmd_and_log {
     #
     $dapscmd =  "$dapsbin --builddir=\"$set_builddir\" --docconfig=\"$dcpath\"";
     $dapscmd .= " --dapsroot=\"$dapsroot\"" if $dapsroot ne "";
+    $dapscmd .= " --debug" if $debug; 
     $dapscmd .= " $format";
     if ( $format =~ /^html.*/ or $format =~ /.*pdf$/ ) {
         $dapscmd .= " --remarks" if $cfg->val("$set", 'remarks');
@@ -218,7 +260,8 @@ sub set_daps_cmd_and_log {
         if ( $format =~ /^html.*/ ) {
             $dapscmd .= " --static";
         }
-    }    
+    }
+    $dapscmd .= " 2>&1" if $debug; 
     #-------------------
     # logfile
     #
@@ -240,7 +283,9 @@ sub rsync {
     my ($rsync_target,$rsync_flags, $set) = @_;
     my %rsync_opts = eval $cfg->val('general', 'rsync_flags');
     my $rsync = File::Rsync->new( \%rsync_opts );
-    my $rsync_src = catdir($builddir, $set);
+    my $rsync_src = catdir("$builddir", "$set");
+
+    print "  Rsyncing $rsync_src to $rsync_target\n" if $verbose;
     
     # run rsync
     $rsync->exec( {
@@ -257,7 +302,7 @@ sub rsync {
             src => "$rsync_src",
             dest => "$rsync_target",
         } );
-        warn "\e[31mFailed to rsnyc $rsync_src to $rsync_target:\n@{$rsync_error}\nrsync command was: @{$rsync_cmd}\e[0m\n";
+        warn "${bcol}Failed to rsnyc $rsync_src to $rsync_target:\n@{$rsync_error}\nrsync command was: @{$rsync_cmd}${ecol}\n";
     }
 }
 
@@ -266,13 +311,16 @@ sub rsync {
 # Help text
 #
 sub usage {
-    print <<EOF
+    print <<EOF;
 Usage: $me --config|-c FILE [OPTIONS]
 
        --clean              : Remove all contents from the builddir specified
                               in the config file
        --config|-c FILE     : Path to config (ini) file
+       --debug              : Turn on DAPS debug mode (very verbose). Implies
+                              verbose mode.
        --help|-h            : This help text
+       --nolocor            : No colored output
        --norsync            : Do not rsync the built documents
        --nosvnup            : Do not update the working directory with svn up
        --sections|s SECTIONS: Comma separated list of sections from ini file
@@ -283,6 +331,8 @@ Usage: $me --config|-c FILE [OPTIONS]
                               --sections SECT1 --sections SECT2
        --verbose|-v         : be verbose (DEBUG mode)  
 EOF
+    exit 0;
+    
 }
 
 
