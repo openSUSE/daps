532b9612883752f2c50a6b63a9a964a5d32ada22
Stefan Knorr <stefan.kpunkt@gmail.com>
2012-09-26 16:02:24 +0000

    HTML/JS: Adapt JS to work even if not all IDs can be found and use a single init() function instead of three for the body
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@2031 2c1a2506-8097-4b87-b405-3e90b480097b

--- contrib/layout/wip/susebooks.js
+++ contrib/layout/wip/susebooks.js
@@ -1,30 +1,32 @@
-                      var deactivatePosition = -1;
+           var deactivatePosition = -1;
            
            function init() {
                if(window.addEventListener) {
                    window.addEventListener("scroll", scrollDeactivator, false);
                }
-           }
-            
-           function show() {
-                document.getElementById('_share-print').style.display = 'block';
-                if ((document.URL.lastIndexOf('http', 0) === 0) || (document.URL.lastIndexOf('spdy', 0) === 0)) {
-                    document.getElementById('_share-print').className = 'online';
-                }
-                return false;
+               if ( document.getElementById('_share-print') ) {
+                 document.getElementById('_share-print').style.display = 'block';
+                 if ((document.URL.lastIndexOf('http', 0) === 0) || (document.URL.lastIndexOf('spdy', 0) === 0)) {
+                     document.getElementById('_share-print').className = 'online';
+                 }
+               }
+               labelInputFind();
+               return false;
             }
             
             function activate(element) {
                 if ((element == '_toc-area') || (element == '_find-area') || (element == '_language-picker' || element == '_format-picker')) {
                     deactivate();
-                    document.getElementById(element).className = 'active';
-                    if ((element == '_find-area')) {
-                        document.getElementById('_find-input').focus();
+                    if ( document.getElementById(element) ) {
+                      document.getElementById(element).className = 'active';
+                      if ((element == '_find-area')) {
+                          document.getElementById('_find-input').focus();
+                      }
+                      else if ((element == '_toc-area')) {
+                          document.getElementById('_find-area').className = 'inactive';
+                      }
+                      document.getElementById(element + '-button').onclick = function() {deactivate(); return false;};
                     }
-                    else if ((element == '_toc-area')) {
-                        document.getElementById('_find-area').className = 'inactive';
-                    }
-                    document.getElementById(element + '-button').onclick = function() {deactivate(); return false;};
                 }
                 else {
                     alert('Eek! The element '+ element +' can\'t be activated.');
@@ -43,14 +45,30 @@
             }
             
             function deactivate() {
-                document.getElementById('_toc-area').className = 'inactive';
-                document.getElementById('_find-area').className = 'active';
-                document.getElementById('_language-picker').className = 'inactive';
-                document.getElementById('_format-picker').className = 'inactive';
-                document.getElementById('_toc-area-button').onclick = function() {activate('_toc-area'); return false;};
-                document.getElementById('_find-area-button').onclick = function() {activate('_find-area'); return false;};
-                document.getElementById('_language-picker-button').onclick = function() {activate('_language-picker'); return false;};
-                document.getElementById('_format-picker-button').onclick = function() {activate('_format-picker'); return false;};
+                if ( document.getElementById('_toc-area') ) {
+                    document.getElementById('_toc-area').className = 'inactive';
+                }
+                if ( document.getElementById('_find-area') ) {
+                    document.getElementById('_find-area').className = 'active';
+                }
+                if ( document.getElementById('_language-picker') ) {
+                    document.getElementById('_language-picker').className = 'inactive';
+                }
+                if ( document.getElementById('_format-picker') ) {
+                    document.getElementById('_format-picker').className = 'inactive';
+                }
+                if ( document.getElementById('_toc-area-button') ) {
+                    document.getElementById('_toc-area-button').onclick = function() {activate('_toc-area'); return false;};
+                }
+                if ( document.getElementById('_find-area-button') ) {
+                    document.getElementById('_find-area-button').onclick = function() {activate('_find-area'); return false;};
+                }
+                if ( document.getElementById('_language-picker-button') ) {
+                    document.getElementById('_language-picker-button').onclick = function() {activate('_language-picker'); return false;};
+                }
+                if ( document.getElementById('_format-picker-button') ) {
+                    document.getElementById('_format-picker-button').onclick = function() {activate('_format-picker'); return false;};
+                }
                 return false;
             }
             
@@ -75,7 +93,7 @@
                     window.open(shareURL, 'sharer', 'toolbar=0,status=0,width=535,height=650');
                 }
                 else {
-                    alert('Eek! The sharing service '+ service +' must be new.');
+                    alert('Eek! The sharing service '+ service +' is new to me.');
                 }
                 
             }
@@ -135,13 +153,17 @@
             }
             
             function unlabelInputFind() {
-                document.getElementById('_find-input-label').style.display = 'none';
+                if ( document.getElementById('_find-input-label') ) {
+                    document.getElementById('_find-input-label').style.display = 'none';
+                }
                 return false;
             }
             
             function labelInputFind() {
-                if (document.getElementById('_find-input').value == '') {
-                    document.getElementById('_find-input-label').style.display = 'block';
+                if ( document.getElementById('_find-input') && document.getElementById('_find-input-label') ) {
+                    if (document.getElementById('_find-input').value == '') {
+                        document.getElementById('_find-input-label').style.display = 'block';
+                    }
                 }
                 return false;
             }
