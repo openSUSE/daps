fc8743a97e291e6636e0c588b4e1651ed88c8731
Thomas Schraitle <tom_schr@web.de>
2013-05-15 07:38:29 +0000

    Added helper script
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@3098 2c1a2506-8097-4b87-b405-3e90b480097b

---
+++ contrib/docbook-xsl-test/mypytest.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# content of myinvoke.py
+import pytest
+import sys
+
+sys.path.insert(0, "tests")
+
+class MyPlugin:
+    def pytest_sessionfinish(self):
+        print("\n*** test run reporting finishing")
+
+pytest.main(plugins=[MyPlugin()])
+
