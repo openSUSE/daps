579d3facfde66ce039d43ff500efdab32adfd28a
Thomas Schraitle <tom_schr@web.de>
2013-05-16 13:55:53 +0000

    Completely revamped and used setup_class(cls) member function to initialize all the necessary objects. Is MUCH more easier than using the test_*() functions with fixtures
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@3126 2c1a2506-8097-4b87-b405-3e90b480097b

--- contrib/docbook-xsl-test/tests/html/article/test_article-sections.py
+++ contrib/docbook-xsl-test/tests/html/article/test_article-sections.py
@@ -5,42 +5,53 @@ from __future__ import print_function
 import os.path
 from lxml import etree
 import pytest
-import glob
 
 
+import conftest as conf
 
-@pytest.mark.incremental
-class TestArticleSections:
-    
-   def test_transform(self, xmltestfile, stylesheets):
-      xmltestfile.parse(stylesheets["html-single"])
-      result = xmltestfile.transform()
-      assert result
-   
-   def test_head(self, xmltestfile, stylesheets):
-      """Checks content of <head> tag (mainly title and meta)
+DIR=os.path.dirname(__file__)
+
+
+class TestArticle():
+   """
+   """
+   @classmethod
+   def setup_class(cls):
+      """ setup class (only once) and transform XML file
       """
-      xmltestfile.parse(stylesheets["html-single"])
-      result = xmltestfile.transform()
-      # ns = xmltestfile.ns
-      #
-      dbtitle = xmltestfile.xml.xpath("/*/title")[0].text
-      restitle=  result.xpath("/html/head/title")[0].text
-      assert restitle == dbtitle
-      #
-      resmeta = result.xpath("/html/head/meta[@name='generator']")[0].attrib.get("content")
-      assert "DocBook XSL Stylesheets" in resmeta
-      
+      cls.xf = conf.XMLFile( os.path.join(DIR, "article.xml") )
+      cls.xf.parse( conf.STYLESHEETS["html-single"] )
+      cls.result = cls.xf.transform()
 
-   def test_div_article(self, xmltestfile, stylesheets):
-      xmltestfile.parse(stylesheets["html-single"])
-      result = xmltestfile.transform()
-      
-      res = result.xpath("/html/body/div[1]")[0].attrib.get("class")
+   def test_head_title(self):
+      """Compare /html/head/title with /*/title
+      """
+      restitle = self.result.xpath("/html/head/title")[0].text
+      dbtitle = self.xf.xml.xpath("/*/title")[0].text
+      assert restitle == dbtitle
+   
+   def test_head_generator(self):
+      """Checks if meta[@name='generator'] contains DocBook string
+      """
+      resmeta = self.result.xpath("/html/head/meta[@name='generator']")[0].attrib.get("content")
+      assert "DocBook XSL Stylesheets" in resmeta
+
+   def test_div_article(self):
+      """Checks if /html/body/div[1]/@class='article'
+      """
+      res = self.result.xpath("/html/body/div[1]")[0].attrib.get("class")
       assert "article" in res
-      
-      res = result.xpath("/html/body/div[@class='article']/div")[0].attrib.get("class")
+
+   div test_div_titlepage(self):
+      """
+      """
+      res = self.result.xpath("/html/body/div[@class='article']/div[@class]")[0].attrib.get("class")
       assert "titlepage" in res
 
-      
-      
+   def test_div_toc(self):
+      """Checks if TOC is written in /html/body[1]/div[1]/div[2]/@class='toc'
+      """
+      res = self.result.xpath("/html/body/div[1]/div[2]")[0].attrib.get("class")
+      assert "toc" in res
+
+# EOF
