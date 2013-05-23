302def580e332575f5ca40381209946829cc0249
Thomas Schraitle <tom_schr@web.de>
2013-05-23 11:55:16 +0000

    Outsourced all the TOC code into a separate TestArticleToc class
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@3156 2c1a2506-8097-4b87-b405-3e90b480097b

--- contrib/docbook-xsl-test/tests/xhtml/article/test_article.py
+++ contrib/docbook-xsl-test/tests/xhtml/article/test_article.py
@@ -12,9 +12,8 @@ import conftest as conf
 DIR=os.path.dirname(__file__)
 
 
-class TestArticle():
-   """
-   """
+class TestArticleToc():
+   """Tests toc """
    @classmethod
    def setup_class(cls):
       """ setup class (only once) and transform XML file
@@ -23,32 +22,7 @@ class TestArticle():
       cls.xf.parse( conf.STYLESHEETS["xhtml-single"] )
       cls.result = cls.xf.transform()
       cls.ns = conf.namespaces()
-
-   def test_head_title(self):
-      """Compare /html/head/title with /*/title
-      """
-      restitle = self.result.xpath("/h:html/h:head/h:title", namespaces=self.ns)[0].text
-      dbtitle = self.xf.xml.xpath("/*/title")[0].text
-      assert restitle == dbtitle
    
-   def test_head_generator(self):
-      """Checks if meta[@name='generator'] contains DocBook string
-      """
-      resmeta = self.result.xpath("/h:html/h:head/h:meta[@name='generator']", namespaces=self.ns)[0].attrib.get("content")
-      assert "DocBook XSL Stylesheets" in resmeta
-
-   def test_div_article(self):
-      """Checks if /html/body/div[1]/@class='article'
-      """
-      res = self.result.xpath("/h:html/h:body/h:div[1]", namespaces=self.ns)[0].attrib.get("class")
-      assert "article" == res
-
-   def test_div_titlepage(self):
-      """Checks for /html/body/div[@class='article']/div[@class] = 'titlepage'
-      """
-      res = self.result.xpath("/h:html/h:body/h:div[@class='article']/h:div[@class]", namespaces=self.ns)[0].attrib.get("class")
-      assert "titlepage" == res
-
    def test_div_toc(self):
       """Checks if TOC is written in /html/body[1]/div[1]/div[2]/@class='toc'
       """
@@ -113,5 +87,44 @@ class TestArticle():
       res = self.result.xpath("/h:html/h:body//h:div[@class='toc']//h:span[@class='sect5']",
                               namespaces=self.ns)
       assert res == []
+   
+
+class TestArticle():
+   """
+   """
+   @classmethod
+   def setup_class(cls):
+      """ setup class (only once) and transform XML file
+      """
+      cls.xf = conf.XMLFile( os.path.join(DIR, "article.xml") )
+      cls.xf.parse( conf.STYLESHEETS["xhtml-single"] )
+      cls.result = cls.xf.transform()
+      cls.ns = conf.namespaces()
+
+   def test_head_title(self):
+      """Compare /html/head/title with /*/title
+      """
+      restitle = self.result.xpath("/h:html/h:head/h:title", namespaces=self.ns)[0].text
+      dbtitle = self.xf.xml.xpath("/*/title")[0].text
+      assert restitle == dbtitle
+   
+   def test_head_generator(self):
+      """Checks if meta[@name='generator'] contains DocBook string
+      """
+      resmeta = self.result.xpath("/h:html/h:head/h:meta[@name='generator']", namespaces=self.ns)[0].attrib.get("content")
+      assert "DocBook XSL Stylesheets" in resmeta
+
+   def test_div_article(self):
+      """Checks if /html/body/div[1]/@class='article'
+      """
+      res = self.result.xpath("/h:html/h:body/h:div[1]", namespaces=self.ns)[0].attrib.get("class")
+      assert "article" == res
+
+   def test_div_titlepage(self):
+      """Checks for /html/body/div[@class='article']/div[@class] = 'titlepage'
+      """
+      res = self.result.xpath("/h:html/h:body/h:div[@class='article']/h:div[@class]", namespaces=self.ns)[0].attrib.get("class")
+      assert "titlepage" == res
+
 
 # EOF
