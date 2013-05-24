8b62ae6157af9375dd15a90079134a50bb490ef5
Thomas Schraitle <tom_schr@web.de>
2013-05-24 08:35:51 +0000

    Extended test with profiling parameter profile.os Was extremly stupid to not quote parameter value correctly. Ahh!
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@3167 2c1a2506-8097-4b87-b405-3e90b480097b

--- contrib/docbook-xsl-test/tests/profiling/test_profiling-book.py
+++ contrib/docbook-xsl-test/tests/profiling/test_profiling-book.py
@@ -12,23 +12,30 @@ import conftest as conf
 DIR=os.path.dirname(__file__)
 
 
-class TestProfilingBook():
-   """Tests book profiling """
+class TestSingleProfilingBook():
+   """Tests book profiling with one profiling parameter"""
    @classmethod
    def setup_class(cls):
       """ setup class (only once) and transform XML file
       """
+      cls.profattr = {'profile.os': "'foo'"}
       cls.xf = conf.XMLFile( os.path.join(DIR, "profiling-book.xml") )
       cls.xf.parse( conf.STYLESHEETS["profile"] )
-      cls.result = cls.xf.transform()
+      cls.result = cls.xf.transform( **cls.profattr )
       cls.ns = conf.namespaces()
 
-   def test_root(self):
+   def test_roots_thesame(self):
+      """Checks, if root element before and after profiling is the same
       """
-      """
-      src = self.xf.xml.xpath("/*", namespaces=self.ns)[0]
-      dest = self.result.xpath("/*", namespaces=self.ns)[0]
+      src = self.xf.xml.getroot()
+      dest = self.result.getroot()
       assert src.tag == dest.tag
 
+   def test_chapter(self):
+      """
+      """
+      src = self.result.xpath("/book/part[@id='singleprof']/chapter")
+      
+      assert len(src) == 2
 
 # EOF
