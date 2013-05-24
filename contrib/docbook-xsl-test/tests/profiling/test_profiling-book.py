4a397f497898bebce09e3a83c412aa2e16d53493
Thomas Schraitle <tom_schr@web.de>
2013-05-24 10:45:09 +0000

    - Added inline profiling test - Renamed class TestSingleProfilingBook -> TestSingleProfilingBookProfileOS - Added doc comments
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@3171 2c1a2506-8097-4b87-b405-3e90b480097b

--- contrib/docbook-xsl-test/tests/profiling/test_profiling-book.py
+++ contrib/docbook-xsl-test/tests/profiling/test_profiling-book.py
@@ -12,7 +12,7 @@ import conftest as conf
 DIR=os.path.dirname(__file__)
 
 
-class TestSingleProfilingBook():
+class TestSingleProfilingBookProfileOS():
    """Tests book profiling with one profiling parameter"""
    @classmethod
    def setup_class(cls):
@@ -32,10 +32,45 @@ class TestSingleProfilingBook():
       assert src.tag == dest.tag
 
    def test_chapter(self):
+      """Checks, if the correct amount of chapters are seen
       """
+      dest = self.result.xpath("/book/part[@id='singleprof']/chapter")
+      assert len(dest) == 2
+   
+   def test_chapter_ids(self):
+      """Checks if available chapter id's are the same
       """
-      src = self.result.xpath("/book/part[@id='singleprof']/chapter")
+      dest = self.result.xpath("/book/part[@id='singleprof']/chapter/@id")
+      src= self.xf.xml.xpath("/book/part[@id='singleprof']/chapter[not(@os!='foo')]/@id")
+      assert src == dest
       
-      assert len(src) == 2
+   def test_chapter_section(self):
+      """Checks, if the correct amount of sect1 are seen
+      """
+      dest = self.result.xpath("//chapter[@id='cha.foo']/sect1")
+      assert len(dest) == 2
+
+   def test_chapter_section_ids(self):
+      """Checks if available sect1 id's are the same
+      """
+      dest = self.result.xpath("//chapter[@id='cha.foo']/sect1/@id")
+      src=self.xf.xml.xpath("//chapter[@id='cha.foo']/sect1[not(@os!='foo')]/@id")
+      assert src == dest
+
+   def test_inline(self):
+      """Checks if correct string is seen
+      """
+      dest = self.result.xpath("string(//sect1[@id='sec.foo.3']/para)").split()
+      dest = " ".join(dest)
+      para = self.xf.xml.xpath("//sect1[@id='sec.foo.3']/para")[0]
+      res=[]
+      for i in para.xpath("node()[not(self::phrase[@os!='foo'])]"):
+         if hasattr(i, "text"):
+            res.append(i.text)
+         else:
+            res.append(i.strip())
+      src = " ".join(res)
+      assert dest == src
+
 
 # EOF
