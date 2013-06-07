6d58cd1966510af479aa8feba9baca9103f6e63e
Frank Sundermeyer <fs@suse.de>
2013-06-07 10:26:19 +0000

    DRAFT and META were missing in PDF-name targets
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@3227 2c1a2506-8097-4b87-b405-3e90b480097b

--- make/print_results.mk
+++ make/print_results.mk
@@ -74,11 +74,11 @@ package-dir-name:
 #
 .PHONY: pdf-name
 pdf-name:
-	@ccecho "result" "$(RESULT_DIR)/$(DOCNAME)-print$(LANGSTRING).pdf"
+	@ccecho "result" "$(RESULT_DIR)/$(DOCNAME)$(DRAFT_STR)$(META_STR)-print$(LANGSTRING).pdf"
 
 .PHONY: color-pdf-name
 color-pdf-name:
-	@ccecho "result" "$(RESULT_DIR)/$(DOCNAME)$(LANGSTRING).pdf"
+	@ccecho "result" "$(RESULT_DIR)/$(DOCNAME)$(DRAFT_STR)$(META_STR)$(LANGSTRING).pdf"
 
 
 #---------------
