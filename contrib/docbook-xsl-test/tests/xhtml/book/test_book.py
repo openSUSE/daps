2926a1baa76fe72611a446e14553bc82ce80ac69
Vanessa Wallfahrer <vwallfahrer@suse.de>
2014-01-24 08:35:11 +0000

    Added new folder for booktests
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@3993 2c1a2506-8097-4b87-b405-3e90b480097b

---
+++ contrib/docbook-xsl-test/tests/xhtml/book/test_book.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import os.path
+from lxml import etree
+import pytest
+
+
+import conftest as conf
+
+DIR=os.path.dirname(__file__)
+
+
+class TestBook():
+   """Tests Book """
+   @classmethod
+   def setup_class(cls):
+       """ setup class (only once) and transform XML file
+       """
+       cls.xf = conf.XMLFile( os.path.join(DIR, "book.xml") )
+       cls.xf.parse( conf.STYLESHEETS["xhtml-single"] )
+       cls.result = cls.xf.transform()
+       cls.ns = conf.namespaces()
+
+   def test_div_class(self):
+        """
+        Checks if  /html/body/div[@class="book"]
+        """
+        res = self.result.xpath("/h:html/h:body/h:div[@class='book']",
+                                namespaces=self.ns)[0]
+        assert res is not None
+
+   def test_div_titlepage(self):
+       """
+       Checks if /html/body/div/div[@class="titlepage"]
+       """
+        
+       tp = self.result.xpath("/h:html/h:body/h:div/h:div[@class='titlepage']",
+                              namespaces=self.ns)[0]
+       assert tp is not None
+
+
