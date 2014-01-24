bd25be243f2a6ed0ab516aa2a4c69497115374cf
Vanessa Wallfahrer <vwallfahrer@suse.de>
2014-01-24 11:52:49 +0000

    Add test_all_sect_title. Does not work
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@3996 2c1a2506-8097-4b87-b405-3e90b480097b

--- contrib/docbook-xsl-test/tests/xhtml/book/test_book.py
+++ contrib/docbook-xsl-test/tests/xhtml/book/test_book.py
@@ -31,7 +31,7 @@ class TestBook():
                                 namespaces=self.ns)[0]
         assert res is not None
 
-   def test_div_titlepage(self):
+   def test_div_book_titlepage(self):
        """
        Checks if /html/body/div[@class='book']/div[@class='titlepage'] is available
        """
@@ -40,4 +40,30 @@ class TestBook():
                               namespaces=self.ns)[0]
        assert tp is not None
 
+   
+class TestBookSections():
+   """Tests Sections """
+   @classmethod
+   def setup_class(cls):
+       """ setup class (only once) and transform XML file
+       """
+       cls.xf = conf.XMLFile( os.path.join(DIR, "book.xml") )
+       cls.xf.parse( conf.STYLESHEETS["xhtml-single"] )
+       cls.result = cls.xf.transform()
+       cls.ns = conf.namespaces()
 
+   def test_all_sect_title(self):
+      """
+      Checks if all sect titles are available:
+      /html/body/div[@class='book']/div[@class='chapter']/div[@class='sect1']//*[@class='title']
+      """
+#/h:html/h:body/h:div[@class='book']/h:div[@class='chapter']/h:div[@class='sect1']
+      restitles = self.result.xpath("//h:*[@class='title']",
+                                namespaces=self.ns)
+      print(">>>",restitles)
+
+      dbtitles = self.xf.xml.xpath("/book/chapter/sect1//title")
+
+      restitles=[ i.text  for i in restitles ]
+      dbtitles=[ i.text for i in dbtitles ]
+      assert restitles == dbtitles
