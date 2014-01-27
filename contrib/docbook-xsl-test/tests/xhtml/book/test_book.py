1fb8ee09da0693cc6dd54605eb9392c6e594eb50
Vanessa Wallfahrer <vwallfahrer@suse.de>
2014-01-27 11:46:49 +0000

    Added new tests: -test_href_chapter: checks, if chapter href is available -test_strong_toc: checks, if table of contents is available -test_href_sects: checks, if sect hrefs are available
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@4000 2c1a2506-8097-4b87-b405-3e90b480097b

--- contrib/docbook-xsl-test/tests/xhtml/book/test_book.py
+++ contrib/docbook-xsl-test/tests/xhtml/book/test_book.py
@@ -54,7 +54,7 @@ class TestBookSections():
 
    def test_all_sect_title(self):
       """
-      Checks if all sect titles are available:
+      Checks, if all sect titles are available:
       /html/body/div[@class='book']/div[@class='chapter']/div[@class='sect1']//*[@class='title']
       """
       restitles = self.result.xpath("/h:html/h:body/h:div[@class='book']/h:div[@class='chapter']/h:div[@class='sect1']//h:*[@class='title']",
@@ -68,4 +68,110 @@ class TestBookSections():
       restitles=[ i.getchildren()[0].tail  for i in restitles ]
       dbtitles =[ i.text for i in dbtitles ]
       assert restitles == dbtitles
+   
+   def test_abstract(self):
+      """
+      Checks, if abstract is available
+      """
+      abstract = self.result.xpath("/h:html/h:body/h:div/h:div/h:div/h:p/h:strong",
+                                   namespaces=self.ns)[0]
+      assert abstract is not None
 
+   
+
+
+
+
+
+
+
+
+
+class TestBookTOC():
+   """Tests the Table of Contents """
+   @classmethod
+   def setup_class(cls):
+       """ setup class (only once) and transform XML file
+       """
+       cls.xf = conf.XMLFile( os.path.join(DIR, "book.xml") )
+       cls.xf.parse( conf.STYLESHEETS["xhtml-single"] )
+       cls.result = cls.xf.transform()
+       cls.ns = conf.namespaces()
+
+   def test_strong_toc(self):
+      """
+      Checks, if the Table of contents is available.
+      """
+      toc = self.result.xpath("/h:html/h:body/h:div[@class='book']/h:div[@class='toc']/h:dl[@class='toc']",
+                              namespaces=self.ns)[0]
+      assert toc is not None
+
+      
+   def test_href_chapter(self):
+      """
+      Checks, if href is available
+      """
+
+      chapterid = self.xf.xml.xpath("/book/chapter/@id")
+
+      assert chapterid 
+      content = chapterid[0]
+
+      href = self.result.xpath("/h:html/h:body/h:div[@class='book']/h:div[@class='toc']/h:dl[@class='toc']/h:dt/h:span[@class='chapter']/h:a[@href='#{0}']".format(content),
+                               namespaces=self.ns)[0]
+      assert href is not None
+
+   def test_href_sects(self):
+      """
+      Checks, if the hrefs are available
+      """
+      sectid = self.xf.xml.xpath("/book/chapter/sect1//@id")
+
+      assert sectid
+      content = sectid[0]
+      
+      href = self.result.xpath("/h:html/h:body/h:div[@class='book']/h:div[@class='toc']/h:dl[@class='toc']/h:dd/h:dl/h:dt/h:span/h:a[@href='#{0}']".format(content),
+                               namespaces=self.ns)
+      
+
+      assert href is not None
+
+
+
+
+
+
+
+
+
+
+
+
+
+#   def test_title_toc(self):
+#      """
+#      Toc gleicher Inhalt wie Titel der sects
+#      """
+#      toctitles = self.result.xpath("/h:html/h:body/h:div[@class='book']/h:div[@cla#ss='toc#']//h:*[@href]",
+#                                    namespaces=self.ns)
+#      secttitles = self.xf.xml.xpath("/book/chapter/@id")
+#
+#      assert len(toctitles) == len(secttitles)
+
+
+
+
+
+ 
+#   def test_href(self):
+#     """Verlinkungen checken 
+#     """
+#     sect1id = self.xf.xml.xpath("/book/chapter/@id")
+#
+#     assert sect1id 
+#     xpath = sect1id[0]
+#     
+#    href = self.result.xpath("/h:html/h:body/h:div[@class='book']/h:div[@class='toc#']//h:a[@href=''#{0}']".format(xpath),
+#                              namespaces=self.ns)[0]
+#     assert href is not None
+     
