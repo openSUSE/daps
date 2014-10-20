930087db802a51fc58b9f15491a112075cbf01e0
Thomas Schraitle <tom_schr@web.de>
2014-10-20 07:50:22 +0000

    Added preliminary structure for our SUSE XSLT testcases
    git-svn-id: svn+ssh://svn.code.sf.net/p/daps/svn/trunk/daps@4277 2c1a2506-8097-4b87-b405-3e90b480097b

---
+++ contrib/suse-xsl-tests/suse-pytest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import pytest
+import sys
+
+#sys.path.insert(0, "tests")
+
+class MyPlugin:
+    def pytest_sessionfinish(self):
+        print("\n*** test run reporting finishing")
+
+
+# Empty statement here needed so minversion reports no error
+pytest
+
+pytest.main(plugins=[MyPlugin()] )
+
+
