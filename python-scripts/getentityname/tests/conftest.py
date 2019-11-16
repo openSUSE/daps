696076db5913dea8836102a08b08e459fd1c7e74
Thomas Schraitle <tom_schr@web.de>
2019-11-16 12:26:25 +0100

    getentityname: Add XML data
    * Add first XML and entity file in tests/data
    * Ignore tests/gen.py

--- python-scripts/getentityname/tests/conftest.py
+++ python-scripts/getentityname/tests/conftest.py
@@ -6,3 +6,40 @@ import pytest
 def add_semver(doctest_namespace):
     # doctest_namespace["gen"] = getentityname
     pass
+
+
+class XMLException(Exception):
+    """Custom exception for error reporting."""
+
+
+class XMLFileItem(pytest.Item):
+    def __init__(self, name, parent):
+        super().__init__(name, parent)
+
+    def runtest(self):
+        print(">>>", self.fspath, self.name, self.listnames)
+              #dir(self))
+        # raise XMLException(self, "Hello")
+        return # ok
+
+    def repr_failure(self, excinfo):
+        if isinstance(excinfo.value, XMLException):
+            return "\n".join(
+                ["XML usecase failed",
+                 "  {!r}".format(*excinfo.value.args),
+                ]
+            )
+
+
+class XMLFileCollector(pytest.File):
+    def collect(self):
+        yield XMLFileItem(str(self.fspath), parent=self)
+
+
+"""
+def pytest_collect_file(path, parent):
+    print(">>>", path, parent)
+    if path.ext == ".xml" and path.basename.endswith("tests/data"):
+        return XMLFileCollector("tests/data", parent=parent)
+"""
+
