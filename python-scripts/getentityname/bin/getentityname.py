b02f1b143064cb54c3ae5aa130761ad3666f222f
Thomas Schraitle <tom_schr@web.de>
2019-11-17 15:15:55 +0100

    getentityname: Revamp script
    * Use version 1.0.0
    * Use argparse
    * Add logging
    * Split regex into dtdmatcher() function
    * Add new remove_xml_comment() function
    * Restructure script into main() and parsecli()
    * Add new test cases

--- python-scripts/getentityname/bin/getentityname.py
+++ python-scripts/getentityname/bin/getentityname.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# $Id: getentityname.py 33777 2008-08-08 09:20:06Z toms $
 #
-# Copyright (C) 2012-2015 SUSE Linux GmbH
+# Copyright (C) 2012-2019 SUSE Linux GmbH
 #
 # Author:
 # Thomas Schraitle <toms at opensuse dot org>
 #
 
+import argparse
 import os.path
+import re
 import sys
-import optparse
+
+import logging
+from logging.config import dictConfig
 
 __proc__ = os.path.basename(sys.argv[0])
-__version__ = "$Revision: 33777 $"[11:-2]
+__version__ = "1.0.0"
 __author__="Thomas Schraitle <thomas DOT schraitle AT suse DOT de>"
 __license__="GPL"
-__doc__= """%(cmd)s [OPTIONS] XMLFILE(S)
-
+__doc__= """
 This script finds every external entity in the internal subset
 of the DTD, for example:
 
@@ -26,23 +28,87 @@ of the DTD, for example:
       "http://www.docbook.org/xml/4.4/docbookx.dtd"
  [
    <!-- The external subset -->
-   <!ENTITY %% entities SYSTEM "entity-decl.ent">
-   %%entities;
-   <!ENTITY %% entities SYSTEM "foo.ent">
-   %%foo.ent;
+   <!ENTITY % entities SYSTEM "entity-decl.ent">
+   %entities;
+   <!ENTITY % entities SYSTEM "foo.ent">
+   %foo.ent;
  ]>
 
 The output will be:
-entity-decl.ent foo.ent
+
+   entity-decl.ent foo.ent
 
 The script detects XML comments inside the internal subset and removes them.
-""".format(cmd=__proc__)
+"""
+
+DEFAULT_LOGGING_DICT = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {'format': '[%(levelname)s] %(funcName)s: %(message)s'},
+    },
+    'handlers': {
+        'default': {
+            'level': 'NOTSET',
+            'formatter': 'standard',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        __name__: {
+            'handlers': ['default'],
+            'level': 'INFO',
+            'propagate': True
+        }
+    }
+}
+
+#: Map verbosity level (int) to log level
+LOGLEVELS = {None: logging.WARNING,  # 0
+             0: logging.WARNING,
+             1: logging.INFO,
+             2: logging.DEBUG,
+             }
+
+#: Instantiate our logger
+log = logging.getLogger(__name__)
+
 
 from xml.sax import make_parser, handler, SAXParseException
 import tempfile
 
 # Contains the filenames found of every external entities:
 resultEntities=[]
+
+###
+# Regular Expressions
+SPACE = r'[ \t\r\n]'                   # whitespace
+S = '%s+'  % SPACE                     # One or more whitespace
+opS = '%s*'% SPACE                     # Zero or more  whitespace
+oS = '%s?' % SPACE                     # Optional whitespace
+NAME = '[a-zA-Z_:][-a-zA-Z0-9._:]*'    # Valid XML name
+QSTR = "(?:'[^']*'|\"[^\"]*\")"        # Quoted XML string
+QUOTES = "(?:'[^']'|\"[^\"]\")"
+SYSTEMLITERAL = '(?P<sysid>%s)' % QSTR
+PUBLICLITERAL = (r'(?P<pubid>"[-\'\(\)+,./:=?;!*#@$_%% \n\ra-zA-Z0-9]*"|'
+                 r"'[-\(\)+,./:=?;!*#@$_%% \n\ra-zA-Z0-9]*')"
+                 )
+EXTERNALID = ("""(?:SYSTEM|PUBLIC{S}{PUBLICLITERAL})"""
+              """{S}{SYSTEMLITERAL}"""
+              ).format(**locals())
+ENTITY = ("""<!ENTITY{S}%{S}"""
+          """(?P<PEDecl>{NAME}){S}"""
+          """{EXTERNALID}{opS}>"""
+          ).format(**locals())
+DOCTYPE = ("""<!DOCTYPE{S}"""
+           "(?P<Name>{NAME})"
+           "("
+             "({S}{EXTERNALID}{opS})?"
+             r"(?:{S}\[(?P<IntSubset>.*)\])?"
+           ")?"
+           """{opS}>"""
+          ).format(**locals())
+
 
 class  MyEntityResolver(handler.EntityResolver):
    """Prints the system identifier of an external entity"""
@@ -53,14 +119,19 @@ class  MyEntityResolver(handler.EntityResolver):
       self.tmpfile=tempfile.NamedTemporaryFile()
       self.filename=os.path.abspath(filename)
       self.ents = ents
+      # log.debug("Created %s", type(self).__name__)
 
    def getEntityList(self):
+      log.debug("Return found entities: %s", self.ents)
       return self.ents
 
    def resolveEntity(self, publicId, systemId):
       """Print the system identifier only, ignoring the public"""
+      # log.debug("resolving entitiy publicID=%r, systemID=%r", publicId, systemId)
       if publicId==None:
         dirname = os.path.dirname(self.filename)
+
+        print("dirname:", dirname)
 
         if not systemId in resultEntities:
             # Remove # to enable absolute paths in self.ents list:
@@ -99,28 +170,30 @@ def joinEnts(unique, sep, ents):
     return sep.join(ents)
 
 
-def getAllEntities(options, filenames):
+def getAllEntities(args):
   """Collects *all* entities in XML files"""
   ents=[]
-  for f in filenames:
+  for f in args.xmlfiles:
+    log.debug("Investigating %s", f)
     if not os.path.exists(f):
-      print("ERROR: File »%s« not found!" % sys.argv[1], file=sys.stderr)
+      log.fatal("ERROR: File »%s« not found!", f)
       sys.exit(10)
+
     parser = make_parser()# ["drv_expat"]
     cwd=os.getcwd()
-    os.chdir(os.path.dirname(f)) 
+    os.chdir(os.path.dirname(f))
+    log.debug("Parser: %s for %r", parser, os.path.basename(f))
     parser.setEntityResolver(MyEntityResolver(f, ents))
     parser.parse(f)
     os.chdir(cwd)
 
-  print(joinEnts(options.unique, options.separator, ents))
+  print(joinEnts(args.unique, args.separator, ents))
 
 
-def getFirstEntity(options, filenames):
-  import re
+def dtdmatcher():
   # Mostly taken from xmllib.py
   # Regular expressions
-  _space = '[ \t\r\n]'                    # whitespace
+  _space = r'[ \t\r\n]'                    # whitespace
   _S = '%s+'  % _space                    # One or more whitespace
   _opS = '%s*'% _space                    # Zero or more  whitespace
   _oS = '%s?' % _space                    # Optional whitespace
@@ -128,53 +201,91 @@ def getFirstEntity(options, filenames):
   _QStr = "(?:'[^']*'|\"[^\"]*\")"        # Quoted XML string
   _quotes = "(?:'[^']'|\"[^\"]\")"
   _SystemLiteral = '(?P<%s>'+_QStr+')'
-  _PublicLiteral = '(?P<%s>"[-\'\(\)+,./:=?;!*#@$_%% \n\ra-zA-Z0-9]*"|' \
-                          "'[-\(\)+,./:=?;!*#@$_%% \n\ra-zA-Z0-9]*')" % 'pubid'
-  _ExternalId = 'SYSTEM' +_S+'('+_SystemLiteral % 'syslit'+')' \
-                '|' + \
-                'PUBLIC'+_S+'('+_PublicLiteral+_S+_SystemLiteral % 'psyslit'+')'
+#  _PublicLiteral = r'(?P<%s>"[-\'\(\)+,./:=?;!*#@$_%% \n\ra-zA-Z0-9]*"|' \
+#                   r"'[-\(\)+,./:=?;!*#@$_%% \n\ra-zA-Z0-9]*')" % 'pubid'
+
+  _PublicLiteral = r'(?P<%s>"[-\'\(\)+,./:=?;!*#@$_%% \n\ra-zA-Z0-9]*"|' \
+                   r"'[-\(\)+,./:=?;!*#@$_%% \n\ra-zA-Z0-9]*')"
+#  _ExternalId = 'SYSTEM' +_S+'('+_SystemLiteral % 'syslit'+')' \
+#                '|' \
+#                + 'PUBLIC'+_S+'('+_PublicLiteral % 'pubid' +_S+_SystemLiteral % 'psyslit'+')'
+#  _ExternalId = '(?:SYSTEM|PUBLIC' + _S +_PublicLiteral % 'pubid'+ ')' \
+#                + _S + _SystemLiteral % 'syslit'
+  _ExternalId = """(?:SYSTEM|PUBLIC{_S}{pubid})
+                   {_S}{syslit}
+                """.format(syslit=_SystemLiteral % 'syslit',
+                           pubid=_PublicLiteral % 'pubid',
+                           psyslit=_SystemLiteral % 'psyslit',
+                           **locals()
+                           )
+
   # Normally, this is the EBNF from the XML Spec:
   # [74] PEDef     ::=      EntityValue | ExternalID
   # As we are only interested in ExternalIDs, we omit EntityValue
   _PEDef = _ExternalId
-  
-  # [72] PEDecl     ::=     '<!ENTITY' S '%' S Name S PEDef S? '>'
-  _PEDecl = '<!ENTITY' + _S + '%' + _S + '(?P<PEDecl>' + \
-            _Name + ')' + _S + _PEDef + _oS + '>'
-  
-  __dtd = r"""<!DOCTYPE""" \
-          r"""%(spc)s+(?P<Name>%(_Name)s)""" \
-          r"""%(spc)s+(?P<Type>PUBLIC|SYSTEM)""" \
-          r"""%(spc)s+(?P<ExternalID>.*)"""  \
-          r"""%(spc)s+\[(?P<IntSubset>.*)\]%(spc)s?>""" % { 
-                   'spc':   _space,
-                   '_Name':    _Name}
 
-  doctype = re.compile(__dtd)
-  entities = re.compile(_PEDecl)
+  # [72] PEDecl     ::=     '<!ENTITY' S '%' S Name S PEDef S? '>'
+  #_PEDecl = '<!ENTITY' + _S + '%' + _S + '(?P<PEDecl>' + \
+  #          _Name + ')' + _S + _PEDef + _oS + '>'
+  _PEDecl = """<!ENTITY{_S}%{_S}
+               (?P<PEDecl>{_Name}){_S}
+               {_ExternalId}{_oS}>
+             """.format(**locals())
+             
+
+#  __dtd = ("<!DOCTYPE"
+#          "{s}+(?P<Name>{_Name})"
+#          "{s}+(?:(?P<Type>PUBLIC|SYSTEM)"
+#          "(?P<ExternalID>.*))?"
+#          # r"{s}+{extid}"
+#          r"""{s}+\[(?P<IntSubset>.*)\]{oS}>"""
+#          ).format(
+#              s=_space,
+#              oS=_oS,
+#              extid=_ExternalId,
+#              _Name=_Name,
+#              )
+#
+# (?:{_S}(?P<ExternalID>.*))?
+  __dtd = r"""<!DOCTYPE{_S}(?P<Name>{_Name}){_S}
+             (?:{_ExternalId}{_opS})?
+             (?:\[(?P<IntSubset>.*)\])?
+             {_oS}>
+          """.format(**locals())
+  doctype = re.compile(__dtd,  re.DOTALL|re.VERBOSE|re.MULTILINE)
+  entities = re.compile(_PEDecl, re.DOTALL|re.VERBOSE|re.MULTILINE)
+
+  return doctype, entities
+
+
+def getFirstEntity(args):
+  doctype, entities = dtdmatcher()
   ents=[]
 
-  for f in filenames:
+  for f in args.xmlfiles:
     # print >> sys.stderr, "Analyzing %s" % f
-    name = open(f, 'r')
-    lines=[]
-    for i in range(50):
-      lines.append( name.readline() )
+    lines = []
+    with open(f, 'r') as fh:
+        for i in range(50):
+            lines.append(fh.readline())
+    log.debug("First two lines: %s%s", lines[0], lines[1])
     lines = "".join(lines)
 
     match_obj = re.search(__dtd, lines,  re.DOTALL)
+    log.debug("")
+    log.debug("Match obj: %s", match_obj)
     if match_obj:
       # Only process, when the internal subset has been found:
       Content = match_obj.group('IntSubset')
       #Name = match_obj.group('Name')
       #Type = match_obj.group('Type')
       #ExternalID = match_obj.group('ExternalID')
-      
+
       #print >> sys.stderr, "Name:    %s" % Name
       #print >> sys.stderr,"Type:    %s" % Type
       #print >> sys.stderr,"ExtId:   %s" % ExternalID
       #print >> sys.stderr,"Content: %s" % Content
-    
+
       if Content:
         # Only process, when there is an internal subset
         # Find comments without reg expressions:
@@ -182,14 +293,14 @@ def getFirstEntity(options, filenames):
         ce = Content.find("-->")
 
         if (cs < 0 and ce >= 0) or (cs >=0 and ce <0):
-          print("ERROR: Wrong XML comment found!", file=sys.stderr)
+          log.fatal("ERROR: Wrong XML comment found!")
           sys.exit(100)
         elif cs >= 0 and ce >= 0:
           # If XML comment found, remove the substring (cs,ce)
           assert cs < ce, "ERROR: Internal error. " \
                 "Start index of XML comment must be smaller than end index!"
           Content = Content[1:cs] +  Content[ce+3:]
-        
+
         # Now it's easy. Search for all entity declarations in the internal subset:
         x=entities.findall(Content)
         for j in x:
@@ -199,63 +310,222 @@ def getFirstEntity(options, filenames):
           else:
             ents.append(j[1])
 
-  print(joinEnts(options.unique, options.separator, ents))
+  print(joinEnts(args.unique, args.separator, ents))
 
 
-def main(cliargs=None):
-    """ """
-    # Create global options parser.
-    #global gparser # only need for 'help' command (optional)
-    #import optparse
-    parser = optparse.OptionParser(__doc__.strip(), \
-                                   version="Revision %s" % __version__[11:-2])
-    parser.add_option("-f", "--only-first",
-        dest="first",
-        action="store_true",
-        help="Never prompt (default %default)")
-    parser.add_option("-u", "--unique",
-        dest="unique",
-        action="store_false",
-        help="Make entity filenames unique (default %default)")
-    parser.add_option("-s", "--separator",
-        dest="separator",
-        help="Set the separator between consecutive filenames (default '%default'). Use '\\n' and '\\t' to insert a CR and TAB character.")
+def remove_xml_comment(content):
+    """Remove first XML comments (<!-- ... -->) in a string
 
-    parser.set_defaults( \
-        first=False,
-        unique=True,
-        separator=' ',
-      )
-    options, args = parser.parse_args(cliargs)
-    if options.separator == '\\n':
-      options.separator = '\n'
-    elif options.separator == '\\t':
-      options.separator = '\t'
- 
+    :param str content: the content with possible XML comments
+    :return: a string without any XML comments
+    :rtype: str
+
+    >>> remove_xml_comment('<!-- hello -->World')
+    'World'
+    >>> remove_xml_comment('<!-- h -->World<!-- is it? -->')
+    'World<!-- is it? -->'
+    """
+    # Only process, when there is an internal subset
+    # Find comments without reg expressions:
+    cs = content.find("<!--")
+    ce = content.find("-->")
+    if (cs < 0 and ce >= 0) or (cs >=0 and ce <0):
+        raise ValueError("ERROR: Wrong XML comment found!")
+          # log.fatal("ERROR: Wrong XML comment found!")
+          # sys.exit(100)
+    elif cs >= 0 and ce >= 0:
+        # If XML comment found, remove the substring (cs,ce)
+        assert cs < ce, "ERROR: Internal error. " \
+            "Start index of XML comment must be smaller than end index!"
+        content = content[1:cs] +  content[ce+3:]
+    return content
+
+
+def getEntities(args, linenr=50):
+    """Read first 50 lines (default) and return any parameter entity names
+
+    :param args:
+    :param int linenr: number of lines that should be investigated
+    :return: a list of all found entities
+    """
+    ents = []
+    seen = set()
+    doctype, entities = dtdmatcher()
+
+    for file in args.xmlfiles:
+        # Prepare the lines:
+        lines = []
+        with open(file, 'r') as fh:
+            for i in range(linenr):
+                lines.append(fh.readline())
+        if len(lines) > 5:
+            log.debug("First five lines:")
+            for i in range(5):
+                log.debug("  line {%d}: %s", i, lines[i].strip())
+        # log.debug("  line 1: %s", lines[0].strip())
+        # log.debug("  line 2: %s", lines[1].strip())
+        lines = "".join(lines)
+
+        # Try to find matches
+        match = doctype.search(lines)
+        log.debug("Match: %s", match)
+        if match:
+            content = match['IntSubset']
+            content = remove_xml_comment(content)
+
+            log.debug("Looking for entities...")
+            for match in entities.finditer(content):
+                # Remove quotes:
+                entity = match['syslit'][1:-1]
+                log.debug("Found entity %r", entity)
+                if entity in seen:
+                    continue
+                seen.add(entity)
+                if entity.startswith("/"):
+                    ents.append(entity)
+                elif entity.startswith("http"):
+                    ents.append(entity)
+                else:
+                    dirname = os.path.dirname(file)
+                    ents.append(os.path.join(dirname, entity))
+                log.debug("ents: %s", ents)
+    # FIXME: Process ents to find other referenced PEs
+    return ents
+
+
+def parsecli(cliargs=None):
+    """Parse CLI with :class:`argparse.ArgumentParser` and return parsed result
+
+    :param list cliargs: Arguments to parse or None (=use sys.argv)
+    :return: parsed CLI result
+    :rtype: :class:`argparse.Namespace`
+    """
+    # Setup logging
+    dictConfig(DEFAULT_LOGGING_DICT)
+
+    parser = argparse.ArgumentParser(description=__doc__.strip(), \
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     usage='%(prog)s [OPTIONS] XMLFILE...')
+    parser.add_argument("--version",
+                        action="version",
+                        version=__version__)
+
+    parser.add_argument('-v', '--verbose',
+                        action="count",
+                        help="Raise verbosity level",
+                        )
+    parser.add_argument("-f", "--only-first",
+                        dest="first",
+                        default=False,
+                        action="store_true",
+                        help="Never prompt (default %(default)s)")
+    parser.add_argument("-u", "--unique",
+                        default=True,
+                        action="store_false",
+                        help="Make entity filenames unique (default %(default)s)")
+    parser.add_argument("-s", "--separator",
+                        default=' ',
+                        help=("Set the separator between consecutive filenames "
+                              "(default '%(default)s'). Use '\\n' and '\\t' to "
+                              "insert a CR and TAB character.")
+                        )
+    parser.add_argument("-t", "--test",
+                        default=False,
+                        action="store_true",
+                        help="Just testing"
+                        )
+    parser.add_argument("xmlfiles",
+                        metavar="XMLFILES",
+                        nargs="+",
+                        help="One or more XML files")
+
+    args = parser.parse_args(cliargs)
+
+    log.setLevel(LOGLEVELS.get(args.verbose, logging.DEBUG))
+    log.debug("CLI args: %s", args)
+    log.debug("")
+    # Save our parser instance:
+    args.parser = parser
+
+    # Fix separators
+    if args.separator == '\\n':
+      args.separator = '\n'
+    elif args.separator == '\\t':
+      args.separator = '\t'
+
     resultargs = []
-    for fn in args:
+    for fn in args.xmlfiles:
         if not os.path.isabs(fn):
             fn = os.path.abspath(fn)
         resultargs.append(fn)
 
-    return parser, options, resultargs
+    args.xmlfiles = resultargs
+
+    return args
+
+
+def test():
+    xml = """<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE chapter
+[
+  <!-- comment -->
+  <!ENTITY % entities SYSTEM "entity-decl.ent">
+   %entities;
+  <!ENTITY % general SYSTEM "general-decl.ent"
+  %general;
+  %
+]>"""
+    doctype, entities = dtdmatcher()
+    # print(">>> doctype:", doctype)
+    match = doctype.search(xml)
+    print(">>> doctype?", match)
+    if match is not None:
+        print("  >>> pos match:", match.groupdict())
+    match = entities.findall(xml, re.DOTALL)
+    print(">>> entities?")
+    if match is not None:
+        print("  >>> pos match:", match)
+    import doctest
+    doctest.testmod(optionflags=doctest.REPORT_NDIFF
+                                |doctest.IGNORE_EXCEPTION_DETAIL
+                                |doctest.NORMALIZE_WHITESPACE)
+
+
+def main(cliargs=None):
+  """Entry point for the application script
+
+    :param list cliargs: Arguments to parse or None (=use :class:`sys.argv`)
+    :return: error code; 0 => everything was succesfull, !=0 => error
+    :rtype: int
+  """
+  try:
+    args = parsecli(cliargs)
+
+    if args.test:
+        test()
+        sys.exit(1)
+
+    if not args:
+      args.parser.print_usage()
+      sys.exit(1)
+
+    ents = getEntities(args)
+    print(joinEnts(args.unique, args.separator, ents))
+    print("-"*40)
+    if args.first:
+      getFirstEntity(args)
+    else:
+      getAllEntities(args)
+    return 0
+
+  except SAXParseException as error:
+      log.fatal(error)
+      print(" ".join(resultEntities))
+  except IOError as error:
+      log.fatal(error)
+
+  return 1
 
 
 if __name__=="__main__":
-  try:
-    p, options, args = main()
-
-    if not args:
-      p.print_usage()
-      sys.exit(1)
-      
-    if options.first:
-      getFirstEntity(options, args)
-    else:
-      getAllEntities(options, args)
-  except SAXParseException as e:
-      print(e, file=sys.stderr)
-      print(" ".join(resultEntities))
-  except IOError as e:
-      print(e, file=sys.stderr)
-
+    sys.exit(main())
--- python-scripts/getentityname/tests/test_script.py
+++ python-scripts/getentityname/tests/test_script.py
@@ -1,3 +1,4 @@
+import re
 import pytest
 
 # "gen" is the abbreviated name for "getentityname.py"
@@ -9,6 +10,270 @@ def test_version(capsys):
     with pytest.raises(SystemExit):
         ver = gen.main(["--version"])
     captured = capsys.readouterr()
-    assert captured.out.rstrip() # == gen.__version__
+    assert captured.out.rstrip() == gen.__version__
 
 
+def test_should_raise_valueerror_in_rm_xml_comment():
+    with pytest.raises(ValueError):
+        gen.remove_xml_comment("<!-- Wrong XML comment")
+
+
+@pytest.mark.parametrize("space", [
+    # space
+    " ",
+    # tab
+    "\t",
+    # newline
+    "\n",
+    # return
+    "\r"
+])
+def test_regex_space(space):
+    assert re.search(gen.SPACE, space)
+
+
+@pytest.mark.parametrize("name", [
+    "a",
+    "A",
+    "abc",
+    "_",
+    "_ab",
+    ":",
+    ":foo",
+    "a:foo",
+])
+def test_regex_name(name):
+    assert re.search(gen.NAME, name)
+
+
+#@pytest.mark.parametrize("qstr", [
+    #"'a'",
+    #'"a"',
+    #"'abc.def'",
+    #"'http://example.org/foo'",
+    #"'http://example.org/foo%20bar#xyz'",
+    #"'http://example.org/foo%20\"bar#xyz'",
+#])
+#def test_regex_qstr(qstr):
+    #assert re.search(gen.QSTR, qstr) 
+
+
+@pytest.mark.parametrize("sysid", [
+    "'a'",
+    '"a"',
+    "'abc.def'",
+    "'http://example.org/foo'",
+    "'http://example.org/foo%20bar#xyz'",
+    "'http://example.org/foo%20\"bar#xyz'",
+])
+def test_regex_systemliteral(sysid):
+    assert re.search(gen.SYSTEMLITERAL, sysid)
+
+
+@pytest.mark.parametrize("pubid", [
+    "'-//OASIS//DTD DocBook XML V4.5//EN'",
+    '"-//OASIS//DTD DocBook XML V4.5//EN"',
+    "'ISO 8879:1986//ENTITIES Added Math Symbols: Arrow Relations//EN//XML'",
+    '"-//W3C//ENTITIES Extra for MathML 2.0//EN"',
+    '"urn:oasis:names:tc:entity:xmlns:xml:catalog"',
+    '"-//OASIS//DTD Entity Resolution XML Catalog V1.0//EN"',
+    '"http://www.w3.org/Graphics/SVG/1.1/rng/svg11.rng"',
+])
+def test_regex_publicliteral(pubid):
+    assert re.search(gen.PUBLICLITERAL, pubid)
+
+
+@pytest.mark.parametrize("extid", [
+    "SYSTEM 'foo.dtd'",
+    'SYSTEM "foo.dtd"',
+    'SYSTEM\n"foo.dtd"',
+    'SYSTEM\t"foo.dtd"',
+    '''PUBLIC "-//OASIS//DTD DocBook XML V4.5//EN" "http://www.oasis-open.org/docbook/xml/4.5/docbookx.dtd"''',
+    '''PUBLIC\n"-//OASIS//DTD DocBook XML V4.5//EN"\n"http://www.oasis-open.org/docbook/xml/4.5/docbookx.dtd"''',
+])
+def test_regex_externalid(extid):
+    assert re.search(gen.EXTERNALID, extid, re.DOTALL|re.VERBOSE|re.MULTILINE)
+
+
+@pytest.mark.parametrize("entity, expected", [
+    # no. 1
+    ("""<!ENTITY % ents SYSTEM "entities.ent">""",
+     {'PEDecl': 'ents', 'pubid': None, 'sysid': '"entities.ent"'}
+     ),
+    # no. 2
+    ("""<!ENTITY\n% ents\nSYSTEM\n"entities.ent"\n >""",
+     {'PEDecl': 'ents', 'pubid': None, 'sysid': '"entities.ent"'}
+     ),
+    # no. 3
+    ("""<!ENTITY % ents PUBLIC "urn:x-test:tom" "http://www.example.org/ent">""",
+     {'PEDecl': 'ents',
+      'pubid': '"urn:x-test:tom"',
+      'sysid': '"http://www.example.org/ent"'}
+     ),
+     # no. 4
+    ("""<!ENTITY\n%\n\tents PUBLIC\t"urn:x-test:tom"\r "http://www.example.org/ent"\n >""",
+     {'PEDecl': 'ents',
+      'pubid': '"urn:x-test:tom"',
+      'sysid': '"http://www.example.org/ent"'}
+     ), 
+])
+def test_regex_entity(entity, expected):
+    match = re.search(gen.ENTITY, entity, re.DOTALL|re.VERBOSE|re.MULTILINE)
+    assert match
+    assert match.groupdict() == expected
+
+
+
+DOCTYPE_TEST_DATA = (
+    # ID-Name, Data, Expected
+    # no. 1
+    ("normal",
+     "<!DOCTYPE chapter>",
+     dict(Name='chapter', pubid=None, sysid=None)
+     ),
+
+    # no. 2
+    ("normal-with-linebreaks",
+     "<!DOCTYPE\nchapter \n>",
+     dict(Name='chapter', pubid=None, sysid=None)
+     ),
+
+    # no. 3
+    ("with-empty-intsubset",
+     "<!DOCTYPE\nchapter []\n>",
+     dict(Name='chapter', pubid=None, sysid=None)
+     ),
+
+    # no. 4
+    ("with-intsubset-oneline",
+    """
+<!DOCTYPE chapter [<!ENTITY % ents SYSTEM "entities.ent">]>""",
+    dict(Name='chapter', pubid=None, sysid=None)
+    ),
+
+    # no. 5
+    ("with-linebreaks",    
+    """
+<!DOCTYPE
+chapter
+[
+  <!ENTITY % ents SYSTEM "entities.ent">
+]
+
+>""",
+    dict(Name='chapter', pubid=None, sysid=None)
+    ),
+
+    # no. 6
+    ("with-xml-declaration",
+     """<?xml version="1.0"?>
+<!DOCTYPE chapter
+[
+  <!ENTITY % ents SYSTEM "entities.ent">
+]>
+     """,
+    dict(Name='chapter', pubid=None, sysid=None)
+    ),
+
+    # no. 7
+    ("with-xml-declaration",
+     """<?xml version="1.0"?>
+<!DOCTYPE chapter SYSTEM "foo.dtd"
+[
+  <!ENTITY % ents SYSTEM "entities.ent">
+]>
+     """,
+    dict(Name='chapter',
+         pubid=None,
+         sysid='"foo.dtd"')
+    ),
+
+    # no. 8
+    ("with-public-identifier",
+     """<!DOCTYPE chapter PUBLIC
+       "-//OASIS//DTD DocBook XML V4.5//EN"
+       "http://www.oasis-open.org/docbook/xml/4.5/docbookx.dtd"
+[
+  <!ENTITY % ents SYSTEM "entities.ent">
+]>
+     """,
+     dict(Name='chapter',
+          pubid='"-//OASIS//DTD DocBook XML V4.5//EN"',
+          sysid='"http://www.oasis-open.org/docbook/xml/4.5/docbookx.dtd"')
+    ),    
+
+    # no. 9
+    ("with-public-identifier",
+     """<!DOCTYPE chapter
+  PUBLIC
+    "-//OASIS//DTD DocBook XML V4.5//EN"
+    "http://www.oasis-open.org/docbook/xml/4.5/docbookx.dtd"
+[
+  <!ENTITY % ents SYSTEM "entities.ent">
+]
+>""",
+     dict(Name='chapter',
+          pubid='"-//OASIS//DTD DocBook XML V4.5//EN"',
+          sysid='"http://www.oasis-open.org/docbook/xml/4.5/docbookx.dtd"')
+    ),    
+
+)
+
+
+@pytest.mark.parametrize("header, expected",
+                         [(data, expected) for _, data, expected in DOCTYPE_TEST_DATA],
+                         ids=[name for name, _, _ in DOCTYPE_TEST_DATA]
+                         )
+def test_regex_doctype(header, expected):
+    match = re.search(gen.DOCTYPE, header, re.VERBOSE|re.DOTALL|re.MULTILINE)
+    assert match
+    resultdict = match.groupdict()
+    del resultdict['IntSubset']
+    assert resultdict == expected
+    
+
+
+ENTITIES_TEST_DATA = (
+    # ID-Name, Data, Expected
+    # no. 1
+    ("system",
+     """<!ENTITY % ent SYSTEM "foo.ent">""",
+     {'PEDecl': 'ent',
+      'syslit': '"foo.ent"',
+      #'psyslit': None,
+      'pubid': None}
+    ),
+    # no. 2
+    ("public",
+     """<!ENTITY % ent PUBLIC "-//TEST//ENTITIES V1.0//EN" "http://example.org/foo.ent">""",
+     {'PEDecl': 'ent',
+      'pubid': '"-//TEST//ENTITIES V1.0//EN"',
+      'syslit': '"http://example.org/foo.ent"',
+      #'syslit': None,
+      }
+    ),
+    # no. 3
+    ("with-linebreaks",
+     """
+     <!ENTITY % here
+     SYSTEM
+  "here.ent">
+     """,
+     {'PEDecl': 'here',
+      'syslit': '"here.ent"',
+      # 'psyslit': None,
+      'pubid': None
+      }
+    ),
+)
+
+@pytest.mark.parametrize("ent, expected",
+                         [(data, expected) for _, data, expected in ENTITIES_TEST_DATA],
+                         ids=[name for name, _, _ in ENTITIES_TEST_DATA]
+                         )
+def test_should_match_entities(ent, expected):
+    _, entities = gen.dtdmatcher()
+    match = entities.search(ent)
+    assert match
+    # print(">>> match:", match.groupdict())    
+    assert match.groupdict() == expected
