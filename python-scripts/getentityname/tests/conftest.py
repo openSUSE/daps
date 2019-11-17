35ffb3968b52341d8aaf2ef258f7c9674969ab62
Thomas Schraitle <tom_schr@web.de>
2019-11-17 15:15:55 +0100

    getentityname: Revamp script
    * Use version 1.0.0
    * Use argparse
    * Add logging
    * Split regex into dtdmatcher() function
    * Add new remove_xml_comment() function
    * Restructure script into main() and parsecli()
    * Add new test cases

--- python-scripts/getentityname/tests/conftest.py
+++ python-scripts/getentityname/tests/conftest.py
@@ -1,31 +1,59 @@
 import pytest
+import py
+import gen
 
-# import getentityname
+ROOTDIR = py.path.local(__file__).parts()[-3]
+TESTDIR = ROOTDIR / "tests"
+DATADIR = TESTDIR / "data"
+BINDIR = ROOTDIR / "bin"
+SCRIPT = BINDIR / "getentityname.py"
+
 
 @pytest.fixture(autouse=True)
 def add_semver(doctest_namespace):
     # doctest_namespace["gen"] = getentityname
     pass
 
+@pytest.fixture
+def doctype():
+    doctype, _ = gen.dtdmatcher()
+    return doctype
 
+
+#@pytest.fixture(scope="session")
+#def xmlgoodcase():
+#
+
+
+## ---------------------------------------------------------------------
 class XMLException(Exception):
     """Custom exception for error reporting."""
 
 
 class XMLFileItem(pytest.Item):
-    def __init__(self, name, parent):
+    def __init__(self, name, parent, failed, error=None):
         super().__init__(name, parent)
+        self.failed = failed
+        self.error = error
 
     def runtest(self):
-        print(">>>", self.fspath, self.name, self.listnames)
-              #dir(self))
-        # raise XMLException(self, "Hello")
+        if self.failed:
+            raise XMLException(self, self.fspath)
         return # ok
 
     def repr_failure(self, excinfo):
         if isinstance(excinfo.value, XMLException):
             return "\n".join(
-                ["XML usecase failed",
+                ["XML usecase {!r} failed".format(self.fspath.relto(ROOTDIR)),
+                 "  error={}".format(self.error),
+                 "  location={}".format(self.location),
+                 # "  name={}".format(self.name),
+                 "  nodeid={}".format(self.nodeid),
+                 "  reportinfo={}".format(self.reportinfo),
+                 "  failed={}".format(self.failed),
+                 "  parent={}".format(self.parent),
+                 "  fspath={}".format(self.fspath),
+                 "{}".format(dir(self)),
                  "  {!r}".format(*excinfo.value.args),
                 ]
             )
@@ -33,13 +61,22 @@ class XMLFileItem(pytest.Item):
 
 class XMLFileCollector(pytest.File):
     def collect(self):
-        yield XMLFileItem(str(self.fspath), parent=self)
+        failed=False
+        excinfo=None
+        try:
+            output = SCRIPT.sysexec(str(self.fspath), shell=True).strip()
+            # failed=True
+            print(">>>", output)
+        except py.process.cmdexec.Error as error:
+            failed=True
+            excinfo = error
+        yield XMLFileItem("entities", parent=self, failed=failed, error=excinfo)
 
 
 """
 def pytest_collect_file(path, parent):
-    print(">>>", path, parent)
-    if path.ext == ".xml" and path.basename.endswith("tests/data"):
-        return XMLFileCollector("tests/data", parent=parent)
+    if path.ext == ".xml" and path.dirname.endswith("tests/data"):
+        print(">>>", path, parent)
+        return XMLFileCollector(path, parent=parent)
 """
 
