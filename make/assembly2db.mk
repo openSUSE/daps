6e7dcc95de70634965ea47be0b974c152ea901c3
Frank Sundermeyer <fs@suse.de>
2022-02-25 17:55:42 +0100

    Adding DocBook assembly support
    assemblies are auto-detected and converted to a bigfile first

---
+++ make/assembly2db.mk
@@ -0,0 +1,43 @@
+# Copyright (C) 2012-2022 SUSE Software Solutions Germany GmbH
+#
+# Author:
+# Frank Sundermeyer <fsundermeyer at opensuse dot org>
+#
+# Convert asciidoc to DocBook5
+#
+# Please submit feedback or patches to
+# <fsundermeyer at opensuse dot org>
+#
+
+include $(DAPSROOT)/make/common_variables.mk
+
+# Stylesheets and Settings
+
+STYLEASSEMBLY  := $(firstword $(wildcard $(addsuffix \
+	        /assembly/assemble.xsl, $(STYLE_ROOTDIRS))))
+
+# used to select one structure among several (if assembly file provides
+# multiple structures)
+# Let's use ROOTID for this purpose
+# NEEDS TO BE FIXED: ASSEMBLY_STRUCTURE and ROOTID are bothe needed
+#
+ifneq "$(strip $(ROOTID))" ""
+  ASSEMBLYSTRINGS := --stringparam "structure.id=$(ROOTID)"
+endif
+
+all: $(MAIN)
+
+$(MAIN): $(ASSEMBLY_MAIN) | $(ASSEMBLY_RESULTDIR)
+  ifeq "$(VERBOSITY)" "2"
+	@ccecho "info"  "   Creating XML bigfile from assembly..."
+  endif
+	$(XSLTPROC) $(ASSEMBLYSTRINGS) $(XSLTPARAM) $(PARAMS) \
+	    $(STRINGPARAMS) --output $@ --stylesheet $(STYLEASSEMBLY) \
+	    --file $< $(XSLTPROCESSOR) $(DEVNULL) \
+	    $(ERR_DEVNULL);
+  ifeq "$(VERBOSITY)" "2"
+	@ccecho "info" "Successfully created XML bigfile $@"
+  endif
+
+$(ASSEMBLY_RESULTDIR):
+	@mkdir -p $@
--- make/clean.mk
+++ make/clean.mk
@@ -14,6 +14,7 @@ clean:
 	rm -rf $(PROFILE_PARENT_DIR)
 	rm -rf $(TMP_DIR)
 	rm -rf $(ADOC_RESULT_DIR)
+	rm -rf $(ASSEMBLY_RESULT_DIR)
 	@ccecho "info" "Successfully removed all profiled and temporary files."
 
 .PHONY: clean-images
