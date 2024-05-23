ca644bcc3d066bf74a093ce2dd19cfa0de998378
Tom Schraitle <toms@suse.de>
2024-05-23 14:53:20 +0200

    Introduce exception classes for config errors

--- python-scripts/metadatavalidator/src/metadatavalidator/exceptions.py
+++ python-scripts/metadatavalidator/src/metadatavalidator/exceptions.py
@@ -1,10 +1,25 @@
 """
 Our custom exception classes
 """
+
 class NoConfigFilesFoundError(FileNotFoundError):
     pass
 
 
+# --- Configuration exceptions
+class BaseConfigError(ValueError):
+    pass
+
+
+class MissingSectionError(BaseConfigError):
+    """A missing section could not be found in the config"""
+
+
+class MissingKeyError(BaseConfigError):
+    """A missing key could not be found in the config"""
+
+
+# --- Validator exceptions
 class BaseMetadataError(ValueError):
     pass
 
