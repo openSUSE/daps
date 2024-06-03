78874faf98ba78f54ecc3f057d2b1e4fd0a6bde9
Tom Schraitle <toms@suse.de>
2024-06-03 16:22:12 +0200

    Test wrong --format argument

--- python-scripts/metadatavalidator/tests/unit/test_script_cli.py
+++ python-scripts/metadatavalidator/tests/unit/test_script_cli.py
@@ -24,3 +24,19 @@ def test_parsecli_config():
     args = parsecli(["--config", "config.ini", "a.xml", "b.xml"])
     assert args.config == "config.ini"
     assert args.xmlfiles == ["a.xml", "b.xml"]
+
+
+def test_parsecli_format():
+    args = parsecli(["--format", "json", "a.xml", "b.xml"])
+    assert args.format == "json"
+    assert args.xmlfiles == ["a.xml", "b.xml"]
+
+
+def test_parsecli_wrong_format(capsys):
+    with pytest.raises(SystemExit,
+                       # match=r".*invalid choice.*"
+                       ):
+        parsecli(["--format", "unknown", "a.xml", "b.xml"])
+
+    caputured = capsys.readouterr()
+    assert "invalid choice: 'unknown'" in caputured.err
