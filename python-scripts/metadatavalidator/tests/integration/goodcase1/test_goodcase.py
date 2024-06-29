f9b6f2a4ad0c1ad1c2dd2431c9afdcc0806b183f
Tom Schraitle <toms@suse.de>
2024-06-29 21:04:58 +0200

    Add another goodcase: entity in doctype

--- python-scripts/metadatavalidator/tests/integration/goodcase1/test_goodcase.py
+++ python-scripts/metadatavalidator/tests/integration/goodcase1/test_goodcase.py
@@ -9,15 +9,29 @@ BASEDIR = os.path.dirname(os.path.realpath(__file__))
 RELATIVE_PATH = os.path.relpath(BASEDIR, os.getcwd())
 
 
-def test_case1_integration(capsys):
+def test_integr_article(capsys):
+    xmlfile = f"{RELATIVE_PATH}/article.xml"
     cli = ["--config", f"{BASEDIR}/config-test.ini",
            "--format", "json",  # needed to avoid formatting issues
-           f"{RELATIVE_PATH}/article.xml"]
+           xmlfile]
 
     result = main(cli)
     captured = capsys.readouterr()
     assert result == 0
     result = json.loads(captured.out)
     assert result[0]['errors'] == []
-    assert result[0]['xmlfile'] == f"{RELATIVE_PATH}/article.xml"
+    assert result[0]['xmlfile'] == xmlfile
 
+
+def test_integr_xml_with_entity_in_doctype(capsys):
+    xmlfile = f"{RELATIVE_PATH}/article-with-entity-in-doctype.xml"
+    cli = ["--config", f"{BASEDIR}/config-test.ini",
+           "--format", "json",  # needed to avoid formatting issues
+           xmlfile]
+
+    result = main(cli)
+    captured = capsys.readouterr()
+    assert result == 0
+    result = json.loads(captured.out)
+    assert result[0]['errors'] == []
+    assert result[0]['xmlfile'] == xmlfile
