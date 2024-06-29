6b498de1f41ad28fab735be25166f841857b9fa9
Tom Schraitle <toms@suse.de>
2024-06-29 21:25:25 +0200

    Replace basic_xmlcontent with tree fixture

--- python-scripts/metadatavalidator/tests/unit/checks/test_check_root.py
+++ python-scripts/metadatavalidator/tests/unit/checks/test_check_root.py
@@ -5,29 +5,12 @@ import pytest
 from metadatavalidator.checks import check_root_tag, check_namespace
 from metadatavalidator.exceptions import InvalidValueError
 
-basic_xmlcontent = """<article xmlns="http://docbook.org/ns/docbook" version="5.2">
-    <info>
-        <title>Test</title>
-    </info>
-    <para/>
-</article>"""
 
-
-def test_check_root_tag():
-    tree = etree.ElementTree(
-        etree.fromstring(basic_xmlcontent,
-                         parser=etree.XMLParser(encoding="UTF-8"))
-    )
-
+def test_check_root_tag(tree):
     assert check_root_tag(tree, {"validator": {"check_root_elements": ["article"]}}) is None
 
 
-def test_check_check_namespace():
-    tree = etree.ElementTree(
-        etree.fromstring(basic_xmlcontent,
-                         parser=etree.XMLParser(encoding="UTF-8"))
-    )
-
+def test_check_check_namespace(tree):
     assert check_namespace(tree, {}) is None
 
 
