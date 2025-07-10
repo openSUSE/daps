e70db9ca3e755b0213878a15dd4129e895ef167a
Frank Sundermeyer <fs@suse.de>
2025-07-10 17:48:31 +0200

    Adding assembly validation via the assembly rng schema

--- make/assembly2db.mk
+++ make/assembly2db.mk
@@ -14,6 +14,7 @@ include $(DAPSROOT)/make/common_variables.mk
 # Stylesheets and Settings
 # Prefer the one shipped with DAPS
 # Can be overwritten in DC files or in shell
+
 STYLEASSEMBLY  ?= $(firstword $(wildcard $(DAPSROOT)/daps-xslt/assembly/assemble.xsl $(addsuffix \
 	            /assembly/assemble.xsl, $(STYLE_ROOTDIRS))))
 
@@ -21,6 +22,11 @@ STYLEASSEMBLY  ?= $(firstword $(wildcard $(DAPSROOT)/daps-xslt/assembly/assemble
 ifeq "$(strip $(STYLEASSEMBLY))" ""
   $(error "FATAL: Could not find assembly/assemble.xsl in the given stylesheet paths. Do you have DocBook >= 5.2 and the respective stylesheets installed?")
 endif
+
+#
+# Stylesheet to list the resources of an assembly
+#
+GETRESOURCES := $(DAPSROOT)/daps-xslt/assembly/find-resources.xsl
 
 # used to select one structure among several (if assembly file provides
 # multiple structures)
@@ -31,9 +37,47 @@ ifneq "$(strip $(STRUCTID))" ""
   ASSEMBLYSTRINGS := --stringparam "structure.id=$(STRUCTID)"
 endif
 
-all: $(MAIN)
+# Get list of all resource files
+#
+# GETRESOURCES produces a list of paths relative to the assembly, e.g.
+# ../common/legal.xml. We will replace ../ with PRJ_DIR
+# use sort to get rid of empty lines
+#
+# Make this a two step process to also generate the empty files that indicate
+# successful validation (located under $(ASSEMBLY_RESULT_DIR))
+# Since resource files are located in several subdirectories, filenames may be
+# the same across subdirectories (foo/bar.xml, bar/bar.xml), therfore we are
+# using the directory name as a prefix: foo_bar.validate bar_bar-validate
+#
 
-$(MAIN): $(ASSEMBLY_MAIN) | $(ASSEMBLY_RESULTDIR)
+# ../common/legal.xml -> $(PRJ_DIR)/common/legal.xml
+#
+ASSEMBLYFILES := $(addprefix $(PRJ_DIR)/,$(subst ../,,$(sort $(shell $(XSLTPROC) --param "header=0" --xinclude --stylesheet $(GETRESOURCES) --file $(ASSEMBLY_MAIN) $(XSLTPROCESSOR) 2>/dev/null ))))
+
+# The list of files we touch to "store" the successful validation result
+# $(PRJ_DIR)/common/legal.xml -> $(ASSEMBLY_RESULT_DIR)/common/legal.avalidate
+# use .avlaidate rather than .validate to not get in conflict with the general
+# .validate rule in make/validate.mk
+#
+ASSEMBLY_VALIDATION_FILES := $(subst $(PRJ_DIR),$(ASSEMBLY_RESULT_DIR),$(subst .xml,.avalidate,$(ASSEMBLYFILES) $(ASSEMBLY_MAIN)))
+
+# we want to mirror the directory structure of the assemblies in ASSEMBLY_RESULT_DIR
+# to make it easy to create a pattern rule for the valildation
+#
+ASSEMBLY_RESULT_SUBDIRS := $(sort $(dir $(ASSEMBLY_VALIDATION_FILES)))
+
+# The main files needs to be validated with jing _and_ xmllint (only the latter
+# catches unresolved entities). Sigh.
+#
+ASSEMBLY_JING_VALIDATION_MAIN := $(subst $(PRJ_DIR),$(ASSEMBLY_RESULT_DIR),$(subst .xml,.jvalidate,$(ASSEMBLY_MAIN)))
+
+
+#
+# Create the big file that is passed as MAIN to the "regular make mechanics
+#
+all: $(MAIN) $(ASSEMBLYFILES)
+
+$(MAIN): $(ASSEMBLY_MAIN) $(ASSEMBLY_JING_VALIDATION_MAIN) $(ASSEMBLY_VALIDATION_FILES) | $(ASSEMBLY_RESULT_DIR)
   ifeq "$(VERBOSITY)" "2"
 	@ccecho "info"  "   Creating XML bigfile from assembly..."
   endif
@@ -45,5 +89,23 @@ $(MAIN): $(ASSEMBLY_MAIN) | $(ASSEMBLY_RESULTDIR)
 	@ccecho "info" "Successfully created XML bigfile $@"
   endif
 
-$(ASSEMBLY_RESULTDIR):
+# Assembly validation
+# see https://github.com/openSUSE/daps/issues/732 for why this is needed
+# We need to validate MAIN (the assembly file) with jing to check whether it
+# adheres to the assembly validation schema
+# We also nee to use plain jing (and not the DAPS provided wrapper script) to
+# be able to detect wrongly placed xi:includes. Additionally assmblies need to
+# be validated with checking IDs
+#
+# the resources files are validated with xmllint fo well-fomedness
+
+$(ASSEMBLY_JING_VALIDATION_MAIN): $(ASSEMBLY_MAIN) | $(ASSEMBLY_RESULT_DIR) $(ASSEMBLY_RESULT_SUBDIRS)
+	jing -i $(ASSEMBLY_RNG) $<
+	touch $@
+
+$(ASSEMBLY_RESULT_DIR)/%.avalidate: $(PRJ_DIR)/%.xml | $(ASSEMBLY_RESULT_DIR) $(ASSEMBLY_RESULT_SUBDIRS)
+	${LIBEXEC_DIR}/daps-xmlwellformed --xinclude $<
+	touch $@
+
+$(ASSEMBLY_RESULT_DIR) $(ASSEMBLY_RESULT_SUBDIRS):
 	@mkdir -p $@
